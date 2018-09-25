5b4d753f90cde423a56919726a88dc4508cfc9b1
David O'Rourke <david@scalefactory.com>
2018-09-25 11:14:36 +0100

    machineactuator: Update Tags and SecurityGroups implementation

--- cloud/aws/services/ec2/tags.go
+++ cloud/aws/services/ec2/tags.go
@@ -86,3 +86,34 @@ func (s *Service) buildTags(clusterName string, lifecycle ResourceLifecycle, add
 
 	return tags
 }
+
+// tagsToMap converts a []*ec2.Tag into a map[string]string.
+func tagsToMap(src []*ec2.Tag) map[string]string {
+	// Create an array of exactly the length we require to hopefully avoid some
+	// allocations while looping.
+	tags := make(map[string]string)
+
+	for _, t := range src {
+		tags[*t.Key] = *t.Value
+	}
+
+	return tags
+}
+
+// mapToTags converts a map[string]string to a []*ec2.Tag
+func mapToTags(src map[string]string) []*ec2.Tag {
+	// Create an array of exactly the length we require to hopefully avoid some
+	// allocations while looping.
+	tags := make([]*ec2.Tag, 0, len(src))
+
+	for k, v := range src {
+		tag := &ec2.Tag{
+			Key:   aws.String(k),
+			Value: aws.String(v),
+		}
+
+		tags = append(tags, tag)
+	}
+
+	return tags
+}
