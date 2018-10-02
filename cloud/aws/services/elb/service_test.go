97fc77fcf6f79afe3b25e6105158fe2f7a553db9
Vince Prignano <vince@vincepri.com>
2018-10-02 11:16:41 -0700

    Add ELB package and mocks
    Signed-off-by: Vince Prignano <vince@vincepri.com>

---
+++ cloud/aws/services/elb/service_test.go
@@ -0,0 +1,32 @@
+// Copyright © 2018 The Kubernetes Authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package elb
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"sigs.k8s.io/cluster-api-provider-aws/cloud/aws/services/elb/mock_elbiface"
+)
+
+func TestNewService(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	elbMock := mock_elbiface.NewMockELBAPI(mockCtrl)
+	svc := NewService(elbMock)
+	if svc == nil {
+		t.Fatalf("Service shouldn't be nil")
+	}
+}
