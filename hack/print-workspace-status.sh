1bc7c335e4fad7a58544c2bea80ba460bd09f1d6
liz <liz@heptio.com>
2018-10-11 13:47:59 -0400

    Improve workspace status

--- hack/print-workspace-status.sh
+++ hack/print-workspace-status.sh
@@ -17,19 +17,62 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-git_commit="$(git describe --tags --always --dirty)"
-build_date="$(date -u '+%Y%m%d')"
-docker_tag="v${build_date}-${git_commit}"
+GIT_COMMIT="$(git describe --always --dirty --abbrev=14)"
 
-# if gitTreeState=$(git status --porcelain 2>/dev/null) && [[ -z ${git_status} ]]; then
-#     KUBE_GIT_TREE_STATE="clean"
-# else
-#     KUBE_GIT_TREE_STATE="dirty"
-# fi
+if git_status=$(git status --porcelain 2>/dev/null) && [[ -z ${git_status} ]]; then
+    GIT_TREE_STATE="clean"
+else
+    GIT_TREE_STATE="dirty"
+fi
+
+# stolen from k8s.io/hack/lib/version.sh
+# Use git describe to find the version based on tags.
+if GIT_VERSION=$(git describe --tags --abbrev=14 2>/dev/null); then
+    # This translates the "git describe" to an actual semver.org
+    # compatible semantic version that looks something like this:
+    #   v1.1.0-alpha.0.6+84c76d1142ea4d
+    #
+    # TODO: We continue calling this "git version" because so many
+    # downstream consumers are expecting it there.
+    DASHES_IN_VERSION=$(echo "${GIT_VERSION}" | sed "s/[^-]//g")
+    if [[ "${DASHES_IN_VERSION}" == "---" ]] ; then
+        # We have distance to subversion (v1.1.0-subversion-1-gCommitHash)
+        GIT_VERSION=$(echo "${GIT_VERSION}" | sed "s/-\([0-9]\{1,\}\)-g\([0-9a-f]\{14\}\)$/.\1\+\2/")
+    elif [[ "${DASHES_IN_VERSION}" == "--" ]] ; then
+        # We have distance to base tag (v1.1.0-1-gCommitHash)
+        GIT_VERSION=$(echo "${GIT_VERSION}" | sed "s/-g\([0-9a-f]\{14\}\)$/+\1/")
+    fi
+    if [[ "${GIT_TREE_STATE}" == "dirty" ]]; then
+        # git describe --dirty only considers changes to existing files, but
+        # that is problematic since new untracked .go files affect the build,
+        # so use our idea of "dirty" from git status instead.
+        GIT_VERSION+="-dirty"
+    fi
+
+
+    # Try to match the "git describe" output to a regex to try to extract
+    # the "major" and "minor" versions and whether this is the exact tagged
+    # version or whether the tree is between two tagged versions.
+    if [[ "${GIT_VERSION}" =~ ^v([0-9]+)\.([0-9]+)(\.[0-9]+)?([-].*)?([+].*)?$ ]]; then
+        GIT_MAJOR=${BASH_REMATCH[1]}
+        GIT_MINOR=${BASH_REMATCH[2]}
+    fi
+
+    # If GIT_VERSION is not a valid Semantic Version, then refuse to build.
+    if ! [[ "${GIT_VERSION}" =~ ^v([0-9]+)\.([0-9]+)(\.[0-9]+)?(-[0-9A-Za-z.-]+)?(\+[0-9A-Za-z.-]+)?$ ]]; then
+        echo "GIT_VERSION should be a valid Semantic Version. Current value: ${GIT_VERSION}"
+        echo "Please see more details here: https://semver.org"
+        exit 1
+    fi
+fi
 
 
 cat <<EOF
 STABLE_DOCKER_REPO ${DOCKER_REPO_OVERRIDE:-gcr.io/cluster-api}
-STABLE_BUILD_GIT_COMMIT ${git_commit}
-DOCKER_TAG ${docker_tag}
+GIT_COMMIT ${GIT_COMMIT-}
+DOCKER_TAG ${docker_tag-}
+GIT_TREE_STATE ${GIT_TREE_STATE-}
+GIT_MAJOR ${GIT_MAJOR-}
+GIT_MINOR ${GIT_MINOR-}
+GIT_VERSION ${GIT_VERSION-}
 EOF
