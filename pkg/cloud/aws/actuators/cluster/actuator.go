64df039a0dffcd625a12fa0fbe7aee3bb2dd8630
Vince Prignano <vince@vincepri.com>
2018-11-20 11:26:22 -0500

    Refactor actuators around a scoped struct (phase 1) (#381)
    Signed-off-by: Vince Prignano <vince@vincepri.com>

--- pkg/cloud/aws/actuators/cluster/actuator.go
+++ pkg/cloud/aws/actuators/cluster/actuator.go
@@ -16,7 +16,7 @@ package cluster
 import (
 	"github.com/pkg/errors"
 	"k8s.io/klog"
-	providerv1 "sigs.k8s.io/cluster-api-provider-aws/pkg/apis/awsprovider/v1alpha1"
+	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/aws/actuators"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/aws/services"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/aws/services/certificates"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/deployer"
@@ -29,88 +29,55 @@ import (
 type Actuator struct {
 	*deployer.Deployer
 
-	clustersGetter client.ClustersGetter
-	servicesGetter services.Getter
+	client client.ClusterV1alpha1Interface
 }
 
 // ActuatorParams holds parameter information for Actuator
 type ActuatorParams struct {
-	ClustersGetter client.ClustersGetter
-	ServicesGetter services.Getter
+	Client client.ClusterV1alpha1Interface
 }
 
 // NewActuator creates a new Actuator
 func NewActuator(params ActuatorParams) *Actuator {
 	res := &Actuator{
-		clustersGetter: params.ClustersGetter,
-		servicesGetter: params.ServicesGetter,
+		client: params.Client,
 	}
 
-	if res.servicesGetter == nil {
-		res.servicesGetter = services.NewSDKGetter()
-	}
-
-	res.Deployer = deployer.New(res.servicesGetter)
+	res.Deployer = deployer.New(services.NewSDKGetter())
 	return res
 }
 
 // Reconcile reconciles a cluster and is invoked by the Cluster Controller
-func (a *Actuator) Reconcile(cluster *clusterv1.Cluster) (reterr error) {
-	klog.Infof("Reconciling cluster %v.", cluster.Name)
+func (a *Actuator) Reconcile(cluster *clusterv1.Cluster) error {
+	klog.Infof("Reconciling cluster %v", cluster.Name)
 
-	// Load provider config.
-	config, err := providerv1.ClusterConfigFromProviderConfig(cluster.Spec.ProviderConfig)
+	scope, err := actuators.NewScope(actuators.ScopeParams{Cluster: cluster, Client: a.client})
 	if err != nil {
-		return errors.Errorf("failed to load cluster provider config: %v", err)
+		return err
 	}
 
-	// Load provider status.
-	status, err := providerv1.ClusterStatusFromProviderStatus(cluster.Status.ProviderStatus)
-	if err != nil {
-		return errors.Errorf("failed to load cluster provider status: %v", err)
-	}
-
-	defer func() {
-		if err := a.storeClusterConfig(cluster, config); err != nil {
-			klog.Errorf("failed to store provider config for cluster %q in namespace %q: %v", cluster.Name, cluster.Namespace, err)
-		}
-
-		if err := a.storeClusterStatus(cluster, status); err != nil {
-			klog.Errorf("failed to store provider status for cluster %q in namespace %q: %v", cluster.Name, cluster.Namespace, err)
-		}
-	}()
+	defer scope.Close()
 
 	// Store some config parameters in the status.
-	status.Region = config.Region
-
-	if len(config.CACertificate) == 0 {
+	if len(scope.ClusterConfig.CACertificate) == 0 {
 		caCert, caKey, err := certificates.NewCertificateAuthority()
 		if err != nil {
 			return errors.Wrap(err, "Failed to generate a CA for the control plane")
 		}
 
-		config.CACertificate = certificates.EncodeCertPEM(caCert)
-		config.CAPrivateKey = certificates.EncodePrivateKeyPEM(caKey)
+		scope.ClusterConfig.CACertificate = certificates.EncodeCertPEM(caCert)
+		scope.ClusterConfig.CAPrivateKey = certificates.EncodePrivateKeyPEM(caKey)
 	}
 
-	// Create new aws session.
-	sess := a.servicesGetter.Session(config)
-
-	// Load ec2 client.
-	ec2 := a.servicesGetter.EC2(sess)
-
-	if err := ec2.ReconcileNetwork(cluster.Name, &status.Network); err != nil {
+	if err := scope.EC2.ReconcileNetwork(cluster.Name, &scope.ClusterStatus.Network); err != nil {
 		return errors.Errorf("unable to reconcile network: %v", err)
 	}
 
-	if err := ec2.ReconcileBastion(cluster.Name, config.SSHKeyName, status); err != nil {
+	if err := scope.EC2.ReconcileBastion(cluster.Name, scope.ClusterConfig.SSHKeyName, scope.ClusterStatus); err != nil {
 		return errors.Errorf("unable to reconcile network: %v", err)
 	}
 
-	// Load elb client.
-	elb := a.servicesGetter.ELB(sess)
-
-	if err := elb.ReconcileLoadbalancers(cluster.Name, &status.Network); err != nil {
+	if err := scope.ELB.ReconcileLoadbalancers(cluster.Name, &scope.ClusterStatus.Network); err != nil {
 		return errors.Errorf("unable to reconcile load balancers: %v", err)
 	}
 
@@ -121,77 +88,26 @@ func (a *Actuator) Reconcile(cluster *clusterv1.Cluster) (reterr error) {
 func (a *Actuator) Delete(cluster *clusterv1.Cluster) error {
 	klog.Infof("Deleting cluster %v.", cluster.Name)
 
-	// Load provider config.
-	config, err := providerv1.ClusterConfigFromProviderConfig(cluster.Spec.ProviderConfig)
+	scope, err := actuators.NewScope(actuators.ScopeParams{Cluster: cluster, Client: a.client})
 	if err != nil {
-		return errors.Errorf("failed to load cluster provider config: %v", err)
+		return err
 	}
 
-	// Load provider status.
-	status, err := providerv1.ClusterStatusFromProviderStatus(cluster.Status.ProviderStatus)
-	if err != nil {
-		return errors.Errorf("failed to load cluster provider status: %v", err)
-	}
+	defer scope.Close()
 
-	// Store some config parameters in the status.
-	status.Region = config.Region
-
-	// Create new aws session.
-	sess := a.servicesGetter.Session(config)
-
-	// Load ec2 client.
-	ec2 := a.servicesGetter.EC2(sess)
-
-	// Load elb client.
-	elb := a.servicesGetter.ELB(sess)
-
-	if err := elb.DeleteLoadbalancers(cluster.Name, &status.Network); err != nil {
+	if err := scope.ELB.DeleteLoadbalancers(cluster.Name, &scope.ClusterStatus.Network); err != nil {
 		return errors.Errorf("unable to delete load balancers: %v", err)
 	}
 
-	if err := ec2.DeleteBastion(cluster.Name, status); err != nil {
+	if err := scope.EC2.DeleteBastion(cluster.Name, scope.ClusterStatus); err != nil {
 		return errors.Errorf("unable to delete bastion: %v", err)
 	}
 
-	if err := ec2.DeleteNetwork(cluster.Name, &status.Network); err != nil {
+	if err := scope.EC2.DeleteNetwork(cluster.Name, &scope.ClusterStatus.Network); err != nil {
 		klog.Errorf("Error deleting cluster %v: %v.", cluster.Name, err)
 		return &controllerError.RequeueAfterError{
 			RequeueAfter: 5 * 1000 * 1000 * 1000,
 		}
-	}
-
-	return nil
-}
-
-func (a *Actuator) storeClusterConfig(cluster *clusterv1.Cluster, config *providerv1.AWSClusterProviderConfig) error {
-	clusterClient := a.clustersGetter.Clusters(cluster.Namespace)
-
-	ext, err := providerv1.EncodeClusterConfig(config)
-	if err != nil {
-		return err
-	}
-
-	cluster.Spec.ProviderConfig.Value = ext
-
-	if _, err := clusterClient.Update(cluster); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (a *Actuator) storeClusterStatus(cluster *clusterv1.Cluster, status *providerv1.AWSClusterProviderStatus) error {
-	clusterClient := a.clustersGetter.Clusters(cluster.Namespace)
-
-	ext, err := providerv1.EncodeClusterStatus(status)
-	if err != nil {
-		return err
-	}
-
-	cluster.Status.ProviderStatus = ext
-
-	if _, err := clusterClient.UpdateStatus(cluster); err != nil {
-		return err
 	}
 
 	return nil
---
+++ pkg/cloud/aws/actuators/scope.go
@@ -0,0 +1,196 @@
+// Copyright © 2018 The Kubernetes Authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package actuators
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/elb"
+	"github.com/pkg/errors"
+	"k8s.io/klog"
+	"sigs.k8s.io/cluster-api-provider-aws/pkg/apis/awsprovider/v1alpha1"
+	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/aws/services"
+	ec2svc "sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/aws/services/ec2"
+	elbsvc "sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/aws/services/elb"
+	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
+	client "sigs.k8s.io/cluster-api/pkg/client/clientset_generated/clientset/typed/cluster/v1alpha1"
+)
+
+// ScopeParams defines the input parameters used to create a new Scope.
+type ScopeParams struct {
+	Cluster *clusterv1.Cluster
+	Client  client.ClusterV1alpha1Interface
+}
+
+// NewScope creates a new Scope from the supplied parameters.
+// This is meant to be called for each different actuator iteration.
+func NewScope(input ScopeParams) (*Scope, error) {
+	if input.Cluster == nil {
+		return nil, errors.New("failed to generate new scope from nil cluster")
+	}
+
+	if input.Cluster.Namespace == "" {
+		return nil, errors.New("failed to generate new scope from cluster with empty namespace")
+	}
+
+	clusterConfig, err := v1alpha1.ClusterConfigFromProviderConfig(input.Cluster.Spec.ProviderConfig)
+	if err != nil {
+		return nil, errors.Errorf("failed to load cluster provider config: %v", err)
+	}
+
+	clusterStatus, err := v1alpha1.ClusterStatusFromProviderStatus(input.Cluster.Status.ProviderStatus)
+	if err != nil {
+		return nil, errors.Errorf("failed to load cluster provider status: %v", err)
+	}
+
+	session, err := session.NewSession(aws.NewConfig().WithRegion(clusterConfig.Region))
+	if err != nil {
+		return nil, errors.Errorf("failed to create aws session: %v", err)
+	}
+
+	// TODO(vincepri): this was probably a temporary hack to pass down the region, it should be revisited.
+	clusterStatus.Region = clusterConfig.Region
+
+	return &Scope{
+		Cluster:       input.Cluster,
+		ClusterClient: input.Client.Clusters(input.Cluster.Namespace),
+		ClusterConfig: clusterConfig,
+		ClusterStatus: clusterStatus,
+		EC2:           ec2svc.NewService(ec2.New(session)),
+		ELB:           elbsvc.NewService(elb.New(session)),
+	}, nil
+}
+
+// Scope defines the basic context for an actuator to operate upon.
+type Scope struct {
+	Cluster       *clusterv1.Cluster
+	ClusterClient client.ClusterInterface
+	ClusterConfig *v1alpha1.AWSClusterProviderConfig
+	ClusterStatus *v1alpha1.AWSClusterProviderStatus
+	EC2           services.EC2Interface
+	ELB           services.ELBInterface
+}
+
+func (s *Scope) storeClusterConfig() error {
+	ext, err := v1alpha1.EncodeClusterConfig(s.ClusterConfig)
+	if err != nil {
+		return err
+	}
+
+	s.Cluster.Spec.ProviderConfig.Value = ext
+
+	if _, err := s.ClusterClient.Update(s.Cluster); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Scope) storeClusterStatus() error {
+	ext, err := v1alpha1.EncodeClusterStatus(s.ClusterStatus)
+	if err != nil {
+		return err
+	}
+
+	s.Cluster.Status.ProviderStatus = ext
+
+	if _, err := s.ClusterClient.UpdateStatus(s.Cluster); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Close closes the current scope persisting the cluster configuration and status.
+func (s *Scope) Close() {
+	if err := s.storeClusterConfig(); err != nil {
+		klog.Errorf("[scope] failed to store provider config for cluster %q in namespace %q: %v", s.Cluster.Name, s.Cluster.Namespace, err)
+	}
+
+	if err := s.storeClusterStatus(); err != nil {
+		klog.Errorf("[scope] failed to store provider status for cluster %q in namespace %q: %v", s.Cluster.Name, s.Cluster.Namespace, err)
+	}
+}
+
+// MachineScope defines a scope defined around a machine and its cluster.
+type MachineScope struct {
+	*Scope
+
+	Machine       *clusterv1.Machine
+	MachineClient client.MachineInterface
+	MachineConfig *v1alpha1.AWSMachineProviderConfig
+	MachineStatus *v1alpha1.AWSMachineProviderStatus
+}
+
+func (m *MachineScope) Close() {
+	defer m.Scope.Close()
+
+	if _, err := m.MachineClient.Update(m.Machine); err != nil {
+		klog.Errorf("[machinescope] failed to update machine: %v", err)
+	}
+
+	if err := m.storeMachineStatus(); err != nil {
+		klog.Errorf("[machinescope] failed to store provider status for machine %q in namespace %q: %v", m.Machine.Name, m.Machine.Namespace, err)
+	}
+}
+
+func (m *MachineScope) storeMachineStatus() error {
+	ext, err := v1alpha1.EncodeMachineStatus(m.MachineStatus)
+	if err != nil {
+		return err
+	}
+
+	m.Machine.Status.ProviderStatus = ext
+
+	if _, err := m.MachineClient.UpdateStatus(m.Machine); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// MachineScopeParams defines the input parameters used to create a new MachineScope.
+type MachineScopeParams struct {
+	Cluster *clusterv1.Cluster
+	Machine *clusterv1.Machine
+	Client  client.ClusterV1alpha1Interface
+}
+
+// NewMachineScope creates a new MachineScope from the supplied parameters.
+// This is meant to be called for each machine actuator operation.
+func NewMachineScope(params MachineScopeParams) (*MachineScope, error) {
+	scope, err := NewScope(ScopeParams{Client: params.Client, Cluster: params.Cluster})
+	if err != nil {
+		return nil, err
+	}
+
+	machineConfig, err := v1alpha1.MachineConfigFromProviderConfig(params.Machine.Spec.ProviderConfig)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get machine config")
+	}
+
+	machineStatus, err := v1alpha1.MachineStatusFromProviderStatus(params.Machine.Status.ProviderStatus)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get machine provider status")
+	}
+
+	return &MachineScope{
+		Scope:         scope,
+		Machine:       params.Machine,
+		MachineClient: params.Client.Machines(params.Cluster.Namespace),
+		MachineConfig: machineConfig,
+		MachineStatus: machineStatus,
+	}, nil
+}
