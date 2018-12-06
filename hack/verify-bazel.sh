8cadc31e8d0ac0331de7c43ed3cca24843409786
Vince Prignano <vince@vincepri.com>
2018-12-06 11:25:05 -0800

    Simplify bazel verify script (#447)
    Signed-off-by: Vince Prignano <vince@vincepri.com>

--- hack/verify-bazel.sh
+++ hack/verify-bazel.sh
@@ -18,21 +18,7 @@ set -o nounset
 set -o pipefail
 set -o verbose
 
-export KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
-source "${KUBE_ROOT}/hack/lib/init.sh"
-
-_tmpdir="$(kube::realpath $(mktemp -d -t verify-bazel.XXXXXX))"
-kube::util::trap_add "rm -rf ${_tmpdir}" EXIT
-
-_tmp_gopath="${_tmpdir}/go"
-_tmp_kuberoot="${_tmp_gopath}/src/sigs.k8s.io/cluster-api-provider-aws"
-mkdir -p "${_tmp_kuberoot}/.."
-cp -a "${KUBE_ROOT}" "${_tmp_kuberoot}/.."
-
-cd "${_tmp_kuberoot}"
-GOPATH="${_tmp_gopath}" PATH="${_tmp_gopath}/bin:${PATH}" "${KUBE_ROOT}/hack/update-bazel.sh"
-
-diff=$(diff -Naupr --exclude=".git" --exclude="bazel-*" -x '_output' "${KUBE_ROOT}" "${_tmp_kuberoot}" || true)
+diff=$(bazel run //:gazelle -- update -mode diff -external vendored)
 
 if [[ -n "${diff}" ]]; then
   echo "${diff}" >&2
