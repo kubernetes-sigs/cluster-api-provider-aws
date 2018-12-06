4bc7a9bd8cb07e98cdbbf04be3652dcfa909a395
Liz <33235082+liztio@users.noreply.github.com>
2018-12-06 13:24:38 -0500

    Add a single, simple ginkgo test: Make sure our set comes up (#440)
    * Add gomega gstruct dep
    
    * Add a single, simple ginkgo test: Make sure our set comes up

---
+++ test/e2e/metacluster_test.go
@@ -0,0 +1,75 @@
+// Copyright 2018 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package e2e_test
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+	"github.com/onsi/gomega/types"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
+	"sigs.k8s.io/cluster-api-provider-aws/test/e2e/util/kind"
+)
+
+const (
+	kindTimeout         = 5 * 60
+	controllerNamespace = "aws-provider-system"
+	controllerName      = "aws-provider-controller-manager"
+)
+
+var _ = Describe("Metacluster", func() {
+	var (
+		cluster kind.Cluster
+		client  kubernetes.Interface
+	)
+	BeforeEach(func() {
+		cluster.Setup()
+		client = cluster.KubeClient()
+	}, kindTimeout)
+
+	AfterEach(func() {
+		cluster.Teardown()
+	})
+
+	Describe("manager container", func() {
+		It("Should be healthy", func() {
+			sets := client.AppsV1().StatefulSets(controllerNamespace)
+
+			Eventually(
+				func() (*appsv1.StatefulSet, error) {
+					return sets.Get(controllerName, metav1.GetOptions{})
+				},
+				2*time.Minute, 5*time.Second,
+			).Should(haveReplicas(1))
+		})
+	})
+})
+
+// haveReplicas matches a stateful set with i replicas
+func haveReplicas(i int32) types.GomegaMatcher {
+	return PointTo(
+		MatchFields(IgnoreExtras, Fields{
+			"Status": MatchFields(IgnoreExtras, Fields{
+				"Replicas":      Equal(i),
+				"ReadyReplicas": Equal(i),
+			}),
+		}),
+	)
+}
