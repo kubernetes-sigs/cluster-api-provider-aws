5ca712a073888310e3715aa47d81670b30a32bb3
Enxebre <alberto.garcial@hotmail.com>
2019-02-01 13:34:20 +0100

    Revendor for actuator-pkg supporting machine.openshift.io

--- vendor/github.com/openshift/cluster-api-actuator-pkg/pkg/e2e/framework/clusters.go
+++ vendor/github.com/openshift/cluster-api-actuator-pkg/pkg/e2e/framework/clusters.go
@@ -5,12 +5,12 @@ import (
 
 	"github.com/golang/glog"
 
+	machinev1beta1 "github.com/openshift/cluster-api/pkg/apis/machine/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
-	machine1beta1 "github.com/openshift/cluster-api/pkg/apis/machine/v1beta1"
 )
 
-func (f *Framework) CreateClusterAndWait(cluster *machine1beta1.Cluster) {
+func (f *Framework) CreateClusterAndWait(cluster *machinev1beta1.Cluster) {
 	f.By(fmt.Sprintf("Creating %q cluster", cluster.Name))
 	err := wait.Poll(PollInterval, PoolTimeout, func() (bool, error) {
 		_, err := f.CAPIClient.MachineV1beta1().Clusters(cluster.Namespace).Create(cluster)
