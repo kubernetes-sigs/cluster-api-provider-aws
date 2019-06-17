e659b4549136b63f628538342ecb7d78491c0b22
erstaples <erstaples@gmail.com>
2019-06-17 10:40:40 -0700

    Add implicit association logic

--- pkg/cloud/aws/services/ec2/subnets.go
+++ pkg/cloud/aws/services/ec2/subnets.go
@@ -202,6 +202,12 @@ func (s *Service) describeVpcSubnets() (v1alpha1.Subnets, error) {
 
 		// ... or if it has an internet route
 		rt := routeTables[*ec2sn.SubnetId]
+		if rt == nil {
+			// If there is no explicit association, subnet defaults to main route table as implicit association
+			if mainRt, ok := routeTables[mainRouteTableInVPCKey]; ok {
+				rt = mainRt
+			}
+		}
 		if rt != nil {
 			spec.RouteTableID = rt.RouteTableId
 			for _, route := range rt.Routes {
