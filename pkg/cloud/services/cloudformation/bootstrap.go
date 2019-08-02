91094ecfdd3829c832e973624c4707490b841b82
guohaowang <wangguohao.2009@gmail.com>
2019-08-02 11:59:47 +0800

    bugfix: add describe-vpc-attribute to permissions
    E0802 03:18:33.901229       1 cluster_controller.go:166] Error reconciling cluster object manage-cluster; failed to reconcile network for cluster manage-cluster: failed to to set vpc attributes for vpc-0a42fc6644d299113: failed to describe vpc attribute: UnauthorizedOperation: You are not authorized to perform this operation.
    	status code: 403, request id: db498bbf-e4ed-45d2-9ab1-140e8f94c9be
    
    Signed-off-by: guohaowang <wangguohao.2009@gmail.com>

--- pkg/cloud/services/cloudformation/bootstrap.go
+++ pkg/cloud/services/cloudformation/bootstrap.go
@@ -179,6 +179,7 @@ func controllersPolicy(accountID string) *iam.PolicyDocument {
 					"ec2:DescribeSecurityGroups",
 					"ec2:DescribeSubnets",
 					"ec2:DescribeVpcs",
+					"ec2:DescribeVpcAttribute",
 					"ec2:DescribeVolumes",
 					"ec2:DetachInternetGateway",
 					"ec2:DisassociateRouteTable",
