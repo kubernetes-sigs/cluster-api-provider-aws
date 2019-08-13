420ea0012d6cec1e1f810b54357d2c0a421837c1
Chuck Ha <chuckh@vmware.com>
2019-08-13 18:11:02 -0400

    capi kubebuilder v2 update
    Signed-off-by: Chuck Ha <chuckh@vmware.com>

--- api/v1alpha2/types.go
+++ api/v1alpha2/types.go
@@ -22,7 +22,7 @@ import (
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha2"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha2"
 )
 
 const (
