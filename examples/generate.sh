f07db98f825cd6d6ec33332f5543eaf51f52adcd
Vince Prignano <vincepri@vmware.com>
2019-08-22 13:14:38 -0700

    :sparkles: Add MachineDeployment example
    Signed-off-by: Vince Prignano <vincepri@vmware.com>

--- examples/generate.sh
+++ examples/generate.sh
@@ -27,8 +27,9 @@ command -v "${ENVSUBST}" >/dev/null 2>&1 || echo -v "Cannot find ${ENVSUBST} in 
 CLUSTERAWSADM=${CLUSTERAWSADM:-clusterawsadm}
 command -v "${CLUSTERAWSADM}" >/dev/null 2>&1 || echo -v "Cannot find ${CLUSTERAWSADM} in path, build it using 'make binaries' in this repository."
 
-# Cluster name.
+# Cluster.
 export CLUSTER_NAME="${CLUSTER_NAME:-test1}"
+export KUBERNETES_VERSION="${KUBERNETES_VERSION:-v1.14.4}"
 
 # Machine settings.
 export CONTROL_PLANE_MACHINE_TYPE="${CONTROL_PLANE_MACHINE_TYPE:-t2.medium}"
@@ -43,6 +44,8 @@ COMPONENTS_AWS_GENERATED_FILE=${SOURCE_DIR}/provider-components/provider-compone
 PROVIDER_COMPONENTS_GENERATED_FILE=${OUTPUT_DIR}/provider-components.yaml
 CLUSTER_GENERATED_FILE=${OUTPUT_DIR}/cluster.yaml
 CONTROLPLANE_GENERATED_FILE=${OUTPUT_DIR}/controlplane.yaml
+MACHINEDEPLOYMENT_GENERATED_FILE=${OUTPUT_DIR}/machinedeployment.yaml
+MACHINES_GENERATED_FILE=${OUTPUT_DIR}/machines.yaml
 
 # Overwrite flag.
 OVERWRITE=0
@@ -92,6 +95,10 @@ echo "Generated ${CLUSTER_GENERATED_FILE}"
 # Generate controlplane resources.
 kustomize build "${SOURCE_DIR}/controlplane" | envsubst > "${CONTROLPLANE_GENERATED_FILE}"
 echo "Generated ${CONTROLPLANE_GENERATED_FILE}"
+
+# Generate machinedeployment resources.
+kustomize build "${SOURCE_DIR}/machinedeployment" | envsubst >> "${MACHINEDEPLOYMENT_GENERATED_FILE}"
+echo "Generated ${MACHINEDEPLOYMENT_GENERATED_FILE}"
 
 # Generate Cluster API provider components file.
 kustomize build "github.com/kubernetes-sigs/cluster-api//config/default/?ref=master" > "${COMPONENTS_CLUSTER_API_GENERATED_FILE}"
