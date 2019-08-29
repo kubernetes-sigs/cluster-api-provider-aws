deb759191b9b81ed49f9609069a14659257a19e8
Chuck Ha <chuckh@vmware.com>
2019-08-29 18:02:15 -0400

    Fix bug with requeuing machines from AWSCluster requests
    Signed-off-by: Chuck Ha <chuckh@vmware.com>

---
+++ controllers/awsmachine_controller_unit_test.go
@@ -0,0 +1,111 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"testing"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/klogr"
+	infrav1 "sigs.k8s.io/cluster-api-provider-aws/api/v1alpha2"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha2"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+)
+
+func setupScheme() (*runtime.Scheme, error) {
+	scheme := runtime.NewScheme()
+	if err := infrav1.AddToScheme(scheme); err != nil {
+		return nil, err
+	}
+	if err := clusterv1.AddToScheme(scheme); err != nil {
+		return nil, err
+	}
+	return scheme, nil
+}
+
+func newMachine(clusterName, machineName string) *clusterv1.Machine {
+	return &clusterv1.Machine{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels: map[string]string{
+				clusterv1.MachineClusterLabelName: clusterName,
+			},
+			Name:      machineName,
+			Namespace: "default",
+		},
+	}
+}
+func newMachineWithInfrastructureRef(clusterName, machineName string) *clusterv1.Machine {
+	m := newMachine(clusterName, machineName)
+	m.Spec.InfrastructureRef = v1.ObjectReference{
+		Kind:       "AWSMachine",
+		Namespace:  "",
+		Name:       "aws" + machineName,
+		APIVersion: infrav1.GroupVersion.String(),
+	}
+	return m
+}
+func newCluster(name string) *clusterv1.Cluster {
+	return &clusterv1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "default",
+		},
+	}
+}
+
+func TestAWSMachineReconciler_AWSClusterToAWSMachines(t *testing.T) {
+	scheme, err := setupScheme()
+	if err != nil {
+		t.Fatal(err)
+	}
+	clusterName := "my-cluster"
+	initObjects := []runtime.Object{
+		newCluster(clusterName),
+		// Create two Machines with an infrastructure ref and one without.
+		newMachineWithInfrastructureRef(clusterName, "my-machine-0"),
+		newMachineWithInfrastructureRef(clusterName, "my-machine-1"),
+		newMachine(clusterName, "my-machine-2"),
+	}
+
+	client := fake.NewFakeClientWithScheme(scheme, initObjects...)
+
+	reconciler := &AWSMachineReconciler{
+		Client: client,
+		Log:    klogr.New(),
+	}
+	requests := reconciler.AWSClusterToAWSMachines(handler.MapObject{
+		Object: &infrav1.AWSCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      clusterName,
+				Namespace: "default",
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						Name:       clusterName,
+						Kind:       "Cluster",
+						APIVersion: clusterv1.GroupVersion.String(),
+					},
+				},
+			},
+		},
+	})
+	if len(requests) != 2 {
+		t.Fatalf("Expected 2 but found %d requests", len(initObjects))
+	}
+}
