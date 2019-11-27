31f97d4679b52e0b061e2c3880791c6ee7b0ce2e
Enxebre <alberto.garcial@hotmail.com>
2019-11-27 20:04:44 +0100

    Revendor to drop openshift/cluster-api

--- go.mod
+++ go.mod
@@ -5,28 +5,44 @@ go 1.12
 require (
 	github.com/aws/aws-sdk-go v1.15.66
 	github.com/blang/semver v3.5.1+incompatible
+	github.com/coreos/etcd v3.3.15+incompatible // indirect
+	github.com/coreos/go-oidc v2.1.0+incompatible // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/ghodss/yaml v1.0.0
-	github.com/go-log/log v0.0.0-20181211034820-a514cf01a3eb // indirect
+	github.com/go-openapi/validate v0.19.2 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
-	github.com/golang/groupcache v0.0.0-20181024230925-c65c006176ff // indirect
 	github.com/golang/mock v1.2.0
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/jonboulle/clockwork v0.1.0 // indirect
 
 	// kube 1.16
-	github.com/openshift/cluster-api v0.0.0-20191004145247-2f02e328bd96
-	github.com/openshift/cluster-api-actuator-pkg v0.0.0-20190527090340-7628df78fb4c
-	github.com/prometheus/client_model v0.0.0-20190129233127-fd36f4220a90 // indirect
-	github.com/prometheus/procfs v0.0.0-20190209105433-f8d8b3f739bd // indirect
+	github.com/openshift/machine-api-operator v0.2.1-0.20191128180243-986b771e661d
+	github.com/sirupsen/logrus v1.4.2 // indirect
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/pflag v1.0.3
 	github.com/stretchr/testify v1.3.0
 	golang.org/x/net v0.0.0-20190812203447-cdfb69ac37fc
-	k8s.io/api v0.0.0-20190918155943-95b840bb6a1f
+	google.golang.org/genproto v0.0.0-20190502173448-54afdca5d873 // indirect
+	google.golang.org/grpc v1.23.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.2.2 // indirect
+	k8s.io/api v0.0.0-20191003000013-35e20aa79eb8
 	k8s.io/apimachinery v0.0.0-20190913080033-27d36303b655
-	k8s.io/client-go v0.0.0-20190918160344-1fbdaa4c8d90
+	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
 	k8s.io/klog v0.4.0
-	sigs.k8s.io/controller-runtime v0.2.0-beta.1.0.20190918234459-801e12a50160
+	k8s.io/utils v0.0.0-20190923111123-69764acb6e8e
+	sigs.k8s.io/controller-runtime v0.3.1-0.20191016212439-2df793d02076
 	sigs.k8s.io/controller-tools v0.2.2-0.20190930215132-4752ed2de7d2
+	sigs.k8s.io/structured-merge-diff v0.0.0-20190817042607-6149e4549fca // indirect
 
 )
 
-replace sigs.k8s.io/controller-runtime => github.com/enxebre/controller-runtime v0.2.0-beta.1.0.20190930160522-58015f7fc885
+replace sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.4.0
+
+// kubernetes-1.16.1
+replace k8s.io/api => k8s.io/api v0.0.0-20191003000013-35e20aa79eb8
+
+replace k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20190913080033-27d36303b655
+
+replace k8s.io/client-go => k8s.io/client-go v0.0.0-20191003000419-f68efa97b39e
---
+++ vendor/github.com/openshift/machine-api-operator/pkg/controller/machine/controller.go
@@ -0,0 +1,413 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package machine
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-log/log/info"
+	commonerrors "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
+	machinev1 "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
+	kubedrain "github.com/openshift/machine-api-operator/pkg/drain"
+	"github.com/openshift/machine-api-operator/pkg/util"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/klog"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	NodeNameEnvVar = "NODE_NAME"
+
+	// ExcludeNodeDrainingAnnotation annotation explicitly skips node draining if set
+	ExcludeNodeDrainingAnnotation = "machine.openshift.io/exclude-node-draining"
+
+	// MachineRegionLabelName as annotation name for a machine region
+	MachineRegionLabelName = "machine.openshift.io/region"
+
+	// MachineAZLabelName as annotation name for a machine AZ
+	MachineAZLabelName = "machine.openshift.io/zone"
+
+	// MachineInstanceStateAnnotationName as annotation name for a machine instance state
+	MachineInstanceStateAnnotationName = "machine.openshift.io/instance-state"
+
+	// MachineInstanceTypeLabelName as annotation name for a machine instance type
+	MachineInstanceTypeLabelName = "machine.openshift.io/instance-type"
+
+	// https://github.com/openshift/enhancements/blob/master/enhancements/machine-instance-lifecycle.md
+	// This is not a transient error, but
+	// indicates a state that will likely need to be fixed before progress can be made
+	// e.g Instance does NOT exist but Machine has providerID/address
+	// e.g Cloud service returns a 4xx response
+	phaseFailed = "Failed"
+
+	// Instance does NOT exist
+	// Machine has NOT been given providerID/address
+	phaseProvisioning = "Provisioning"
+
+	// Instance exists
+	// Machine has been given providerID/address
+	// Machine has NOT been given nodeRef
+	phaseProvisioned = "Provisioned"
+
+	// Instance exists
+	// Machine has been given providerID/address
+	// Machine has been given a nodeRef
+	phaseRunning = "Running"
+
+	// Machine has a deletion timestamp
+	phaseDeleting = "Deleting"
+)
+
+var DefaultActuator Actuator
+
+func AddWithActuator(mgr manager.Manager, actuator Actuator) error {
+	return add(mgr, newReconciler(mgr, actuator))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager, actuator Actuator) reconcile.Reconciler {
+	r := &ReconcileMachine{
+		Client:        mgr.GetClient(),
+		eventRecorder: mgr.GetEventRecorderFor("machine-controller"),
+		config:        mgr.GetConfig(),
+		scheme:        mgr.GetScheme(),
+		actuator:      actuator,
+	}
+	return r
+}
+
+func stringPointerDeref(stringPointer *string) string {
+	if stringPointer != nil {
+		return *stringPointer
+	}
+	return ""
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("machine_controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to Machine
+	return c.Watch(
+		&source.Kind{Type: &machinev1.Machine{}},
+		&handler.EnqueueRequestForObject{},
+	)
+}
+
+// ReconcileMachine reconciles a Machine object
+type ReconcileMachine struct {
+	client.Client
+	config *rest.Config
+	scheme *runtime.Scheme
+
+	eventRecorder record.EventRecorder
+
+	actuator Actuator
+}
+
+// Reconcile reads that state of the cluster for a Machine object and makes changes based on the state read
+// and what is in the Machine.Spec
+// +kubebuilder:rbac:groups=machine.openshift.io,resources=machines;machines/status,verbs=get;list;watch;create;update;patch;delete
+func (r *ReconcileMachine) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	// TODO(mvladev): Can context be passed from Kubebuilder?
+	ctx := context.TODO()
+
+	// Fetch the Machine instance
+	m := &machinev1.Machine{}
+	if err := r.Client.Get(ctx, request.NamespacedName, m); err != nil {
+		if apierrors.IsNotFound(err) {
+			// Object not found, return.  Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			return reconcile.Result{}, nil
+		}
+
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	// Implement controller logic here
+	name := m.Name
+	klog.Infof("Reconciling Machine %q", name)
+
+	if errList := m.Validate(); len(errList) > 0 {
+		err := fmt.Errorf("%q machine validation failed: %v", m.Name, errList.ToAggregate().Error())
+		klog.Error(err)
+		r.eventRecorder.Eventf(m, corev1.EventTypeWarning, "FailedValidate", err.Error())
+		return reconcile.Result{}, err
+	}
+
+	// If object hasn't been deleted and doesn't have a finalizer, add one
+	// Add a finalizer to newly created objects.
+	if m.ObjectMeta.DeletionTimestamp.IsZero() {
+		finalizerCount := len(m.Finalizers)
+
+		if !util.Contains(m.Finalizers, machinev1.MachineFinalizer) {
+			m.Finalizers = append(m.ObjectMeta.Finalizers, machinev1.MachineFinalizer)
+		}
+
+		if len(m.Finalizers) > finalizerCount {
+			if err := r.Client.Update(ctx, m); err != nil {
+				klog.Infof("Failed to add finalizers to machine %q: %v", name, err)
+				return reconcile.Result{}, err
+			}
+
+			// Since adding the finalizer updates the object return to avoid later update issues
+			return reconcile.Result{}, nil
+		}
+	}
+
+	if !m.ObjectMeta.DeletionTimestamp.IsZero() {
+		if err := r.setPhase(m, phaseDeleting, ""); err != nil {
+			return reconcile.Result{}, err
+		}
+
+		// no-op if finalizer has been removed.
+		if !util.Contains(m.ObjectMeta.Finalizers, machinev1.MachineFinalizer) {
+			klog.Infof("Reconciling machine %q causes a no-op as there is no finalizer", name)
+			return reconcile.Result{}, nil
+		}
+
+		klog.Infof("Reconciling machine %q triggers delete", name)
+		// Drain node before deletion
+		// If a machine is not linked to a node, just delete the machine. Since a node
+		// can be unlinked from a machine when the node goes NotReady and is removed
+		// by cloud controller manager. In that case some machines would never get
+		// deleted without a manual intervention.
+		if _, exists := m.ObjectMeta.Annotations[ExcludeNodeDrainingAnnotation]; !exists && m.Status.NodeRef != nil {
+			if err := r.drainNode(m); err != nil {
+				klog.Errorf("Failed to drain node for machine %q: %v", name, err)
+				return delayIfRequeueAfterError(err)
+			}
+		}
+
+		if err := r.actuator.Delete(ctx, m); err != nil {
+			// isInvalidMachineConfiguration will take care of the case where the
+			// configuration is invalid from the beginning. len(m.Status.Addresses) > 0
+			// will handle the case when a machine configuration was invalidated
+			// after an instance was created. So only a small window is left when
+			// we can loose instances, e.g. right after request to create one
+			// was sent and before a list of node addresses was set.
+			if len(m.Status.Addresses) > 0 || !isInvalidMachineConfigurationError(err) {
+				klog.Errorf("Failed to delete machine %q: %v", name, err)
+				return delayIfRequeueAfterError(err)
+			}
+		}
+
+		if m.Status.NodeRef != nil {
+			klog.Infof("Deleting node %q for machine %q", m.Status.NodeRef.Name, m.Name)
+			if err := r.deleteNode(ctx, m.Status.NodeRef.Name); err != nil {
+				klog.Errorf("Error deleting node %q for machine %q", name, err)
+				return reconcile.Result{}, err
+			}
+		}
+
+		// Remove finalizer on successful deletion.
+		m.ObjectMeta.Finalizers = util.Filter(m.ObjectMeta.Finalizers, machinev1.MachineFinalizer)
+		if err := r.Client.Update(context.Background(), m); err != nil {
+			klog.Errorf("Failed to remove finalizer from machine %q: %v", name, err)
+			return reconcile.Result{}, err
+		}
+
+		klog.Infof("Machine %q deletion successful", name)
+		return reconcile.Result{}, nil
+	}
+
+	if machineIsFailed(m) {
+		klog.Warningf("Machine %q has gone %q phase. It won't reconcile", name, phaseFailed)
+		return reconcile.Result{}, nil
+	}
+
+	instanceExists, err := r.actuator.Exists(ctx, m)
+	if err != nil {
+		klog.Errorf("Failed to check if machine %q exists: %v", name, err)
+		return reconcile.Result{}, err
+	}
+
+	if instanceExists {
+		klog.Infof("Reconciling machine %q triggers idempotent update", name)
+		if err := r.actuator.Update(ctx, m); err != nil {
+			klog.Errorf(`Error updating machine "%s/%s": %v`, m.Namespace, name, err)
+			return delayIfRequeueAfterError(err)
+		}
+
+		if !machineIsProvisioned(m) {
+			klog.Errorf(`Instance for Machine "%s/%s exists but providerID or addresses has not been given to the machine yet"`, m.Namespace, name)
+			return reconcile.Result{}, err
+		}
+		if machineHasNode(m) {
+			if err := r.setPhase(m, phaseRunning, ""); err != nil {
+				return reconcile.Result{}, err
+			}
+		} else {
+			if err := r.setPhase(m, phaseProvisioned, ""); err != nil {
+				return reconcile.Result{}, err
+			}
+		}
+		return reconcile.Result{}, nil
+	}
+
+	// Instance does not exist but the machine has been given a providerID/address.
+	// This can only be reached if an instance was deleted outside the machine API
+	if machineIsProvisioned(m) {
+		if err := r.setPhase(m, phaseFailed, "Can't find created instance."); err != nil {
+			return reconcile.Result{}, err
+		}
+		return reconcile.Result{}, nil
+	}
+
+	// Machine resource created and instance does not exist yet.
+	if err := r.setPhase(m, phaseProvisioning, ""); err != nil {
+		return reconcile.Result{}, err
+	}
+	klog.Infof("Reconciling machine object %v triggers idempotent create.", m.ObjectMeta.Name)
+	if err := r.actuator.Create(ctx, m); err != nil {
+		klog.Warningf("Failed to create machine %q: %v", name, err)
+		if isInvalidMachineConfigurationError(err) {
+			if err := r.setPhase(m, phaseFailed, err.Error()); err != nil {
+				return reconcile.Result{}, err
+			}
+			return reconcile.Result{}, nil
+		}
+		return delayIfRequeueAfterError(err)
+	}
+
+	return reconcile.Result{}, nil
+}
+
+func (r *ReconcileMachine) drainNode(machine *machinev1.Machine) error {
+	kubeClient, err := kubernetes.NewForConfig(r.config)
+	if err != nil {
+		return fmt.Errorf("unable to build kube client: %v", err)
+	}
+	node, err := kubeClient.CoreV1().Nodes().Get(machine.Status.NodeRef.Name, metav1.GetOptions{})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// If an admin deletes the node directly, we'll end up here.
+			klog.Infof("Could not find node from noderef, it may have already been deleted: %v", machine.Status.NodeRef.Name)
+			return nil
+		}
+		return fmt.Errorf("unable to get node %q: %v", machine.Status.NodeRef.Name, err)
+	}
+
+	if err := kubedrain.Drain(
+		kubeClient,
+		[]*corev1.Node{node},
+		&kubedrain.DrainOptions{
+			Force:              true,
+			IgnoreDaemonsets:   true,
+			DeleteLocalData:    true,
+			GracePeriodSeconds: -1,
+			Logger:             info.New(klog.V(0)),
+			// If a pod is not evicted in 20 second, retry the eviction next time the
+			// machine gets reconciled again (to allow other machines to be reconciled)
+			Timeout: 20 * time.Second,
+		},
+	); err != nil {
+		// Machine still tries to terminate after drain failure
+		klog.Warningf("drain failed for machine %q: %v", machine.Name, err)
+		return &RequeueAfterError{RequeueAfter: 20 * time.Second}
+	}
+
+	klog.Infof("drain successful for machine %q", machine.Name)
+	r.eventRecorder.Eventf(machine, corev1.EventTypeNormal, "Deleted", "Node %q drained", node.Name)
+
+	return nil
+}
+
+func (r *ReconcileMachine) deleteNode(ctx context.Context, name string) error {
+	var node corev1.Node
+	if err := r.Client.Get(ctx, client.ObjectKey{Name: name}, &node); err != nil {
+		if apierrors.IsNotFound(err) {
+			klog.V(2).Infof("Node %q not found", name)
+			return nil
+		}
+		klog.Errorf("Failed to get node %q: %v", name, err)
+		return err
+	}
+	return r.Client.Delete(ctx, &node)
+}
+
+func delayIfRequeueAfterError(err error) (reconcile.Result, error) {
+	switch t := err.(type) {
+	case *RequeueAfterError:
+		klog.Infof("Actuator returned requeue-after error: %v", err)
+		return reconcile.Result{Requeue: true, RequeueAfter: t.RequeueAfter}, nil
+	}
+	return reconcile.Result{}, err
+}
+
+func isInvalidMachineConfigurationError(err error) bool {
+	switch t := err.(type) {
+	case *MachineError:
+		if t.Reason == commonerrors.InvalidConfigurationMachineError {
+			klog.Infof("Actuator returned invalid configuration error: %v", err)
+			return true
+		}
+	}
+	return false
+}
+
+func (r *ReconcileMachine) setPhase(machine *machinev1.Machine, phase string, errorMessage string) error {
+	if stringPointerDeref(machine.Status.Phase) != phase {
+		klog.V(3).Infof("Machine %q going into phase %q", machine.GetName(), phase)
+		baseToPatch := client.MergeFrom(machine.DeepCopy())
+		machine.Status.Phase = &phase
+		machine.Status.ErrorMessage = nil
+		now := metav1.Now()
+		machine.Status.LastUpdated = &now
+		if phase == phaseFailed && errorMessage != "" {
+			machine.Status.ErrorMessage = &errorMessage
+		}
+		if err := r.Client.Status().Patch(context.Background(), machine, baseToPatch); err != nil {
+			klog.Errorf("Failed to update machine %q: %v", machine.GetName(), err)
+			return err
+		}
+	}
+	return nil
+}
+
+func machineIsProvisioned(machine *machinev1.Machine) bool {
+	return len(machine.Status.Addresses) > 0 || stringPointerDeref(machine.Spec.ProviderID) != ""
+}
+
+func machineHasNode(machine *machinev1.Machine) bool {
+	return machine.Status.NodeRef != nil
+}
+
+func machineIsFailed(machine *machinev1.Machine) bool {
+	if stringPointerDeref(machine.Status.Phase) == phaseFailed {
+		return true
+	}
+	return false
+}
