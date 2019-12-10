23f1114156c84ba76d9ef5a665089210694bd607
Vince Prignano <vincepri@vmware.com>
2019-12-10 14:17:58 -0800

    :bug: Fix e2e CABPK imports
    Signed-off-by: Vince Prignano <vincepri@vmware.com>

--- test/e2e/aws_test.go
+++ test/e2e/aws_test.go
@@ -40,7 +40,7 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	infrav1 "sigs.k8s.io/cluster-api-provider-aws/api/v1alpha3"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha3"
-	bootstrapv1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1alpha2"
+	bootstrapv1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1alpha3"
 	kubeadmv1beta1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/types/v1beta1"
 	capiFlag "sigs.k8s.io/cluster-api/test/helpers/flag"
 	"sigs.k8s.io/cluster-api/util"
