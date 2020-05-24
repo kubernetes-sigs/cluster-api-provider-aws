cb271d1749827a0341cc68ae59e11fec1c4b4cc9
Andrew Myhre <andrew.myhre@capitalone.com>
2020-05-24 11:51:58 -0400

    add networkspec conversion function

--- api/v1alpha2/awscluster_conversion.go
+++ api/v1alpha2/awscluster_conversion.go
@@ -201,7 +201,9 @@ func Convert_v1alpha3_VPCSpec_To_v1alpha2_VPCSpec(in *infrav1alpha3.VPCSpec, out
 	return autoConvert_v1alpha3_VPCSpec_To_v1alpha2_VPCSpec(in, out, s)
 }
 
-//Convert_v1alpha3_NetworkSpec_To_v1alpha2_NetworkSpec
-func Convert_v1alpha3_NetworkSpec_To_v1alpha2_NetworkSpec(in *infrav1alpha3.NetworkSpec, out *NetworkSpec, s apiconversion.Scope) error {
-	return autoConvert_v1alpha3_NetworkSpec_To_v1alpha2_NetworkSpec(in, out, s)
+func Convert_v1alpha3_NetworkSpec_To_v1alpha2_NetworkSpec(in *infrav1alpha3.NetworkSpec, out *NetworkSpec, s apiconversion.Scope) error { //nolint
+	if err := autoConvert_v1alpha3_NetworkSpec_To_v1alpha2_NetworkSpec(in, out, s); err != nil {
+		return err
+	}
+	return nil
 }
