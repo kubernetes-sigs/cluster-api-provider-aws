618dafd7399286ad9152c2ec3086273d43b327e4
Michael Beaumont <mjboamail@gmail.com>
2020-10-07 12:44:19 +0200

    Improve AWSManagedMachinePool remote access support

--- exp/api/v1alpha3/awsmanagedmachinepool_types.go
+++ exp/api/v1alpha3/awsmanagedmachinepool_types.go
@@ -124,7 +124,8 @@ type ManagedMachinePoolScaling struct {
 
 // ManagedRemoteAccess specifies remote access settings for EC2 instances
 type ManagedRemoteAccess struct {
-	// SSHKeyName specifies which EC2 SSH key can be used to access machines
+	// SSHKeyName specifies which EC2 SSH key can be used to access machines.
+	// If left empty, the key from the control plane is used.
 	SSHKeyName *string `json:"sshKeyName,omitempty"`
 
 	// SourceSecurityGroups specifies which security groups are allowed access
