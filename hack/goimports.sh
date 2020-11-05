848cfab0b5ec24839c16f1b7b4e3f6e85a0dd965
Danil-Grigorev <dgrigore@redhat.com>
2020-11-05 13:50:21 +0100

    Add verify-diff check in generate task

--- hack/goimports.sh
+++ hack/goimports.sh
@@ -5,7 +5,6 @@ if [ "$IS_CONTAINER" != "" ]; then
   for TARGET in "${@}"; do
     find "${TARGET}" -name '*.go' ! -path '*/vendor/*' ! -path '*/.build/*' -exec goimports -w {} \+
   done
-  git diff --exit-code
 else
   docker run -it --rm \
     --env IS_CONTAINER=TRUE \
