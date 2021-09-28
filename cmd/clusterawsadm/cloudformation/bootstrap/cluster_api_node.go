7e2b4de23ed9c23f74c5b45c8a0ff6ec2d39f831
shivi28 <sishivani@vmware.com>
2021-09-28 23:08:28 +0530

    Update clusterawsadm for v1beta1 support

--- cmd/clusterawsadm/cloudformation/bootstrap/cluster_api_node.go
+++ cmd/clusterawsadm/cloudformation/bootstrap/cluster_api_node.go
@@ -17,7 +17,7 @@ limitations under the License.
 package bootstrap
 
 import (
-	infrav1 "sigs.k8s.io/cluster-api-provider-aws/api/v1alpha4"
+	infrav1 "sigs.k8s.io/cluster-api-provider-aws/api/v1beta1"
 )
 
 func (t Template) secretPolicy(secureSecretsBackend infrav1.SecretBackend) infrav1.StatementEntry {
