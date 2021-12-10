c7b919db38929c702aaced8b94f23ee5e1a52938
Sedef Savas <ssavas@ucdavis.edu>
2021-12-10 03:53:24 -0800

    Remove unnecessary log from AWSInstanceState controller

--- exp/instancestate/awsinstancestate_controller.go
+++ exp/instancestate/awsinstancestate_controller.go
@@ -87,8 +87,8 @@ func (r *AwsInstanceStateReconciler) Reconcile(ctx context.Context, req ctrl.Req
 
 	// Handle deleted clusters
 	if !awsCluster.DeletionTimestamp.IsZero() {
-		r.Log.Info("cluster deleted, removing queue URL", "cluster", awsCluster.Name)
 		r.queueURLs.Delete(req.Name)
+		return reconcile.Result{}, nil
 	}
 
 	// retrieve queue URL if it isn't already tracked
