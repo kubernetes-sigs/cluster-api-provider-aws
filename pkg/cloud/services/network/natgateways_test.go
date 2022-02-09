8afcd0be89184b027fe4327626bdd1299a8e46b0
Madhur Agarwal <agarwalma@vmware.com>
2022-02-09 22:26:54 +0530

    natgateway unit tests

--- pkg/cloud/services/network/natgateways_test.go
+++ pkg/cloud/services/network/natgateways_test.go
@@ -23,11 +23,13 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/golang/mock/gomock"
+	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	infrav1 "sigs.k8s.io/cluster-api-provider-aws/api/v1beta1"
+	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/awserrors"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/scope"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/services/ec2/mock_ec2iface"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
@@ -403,4 +405,326 @@ func TestReconcileNatGateways(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestDeleteNatGateways(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	testCases := []struct {
+		name           string
+		input          []infrav1.SubnetSpec
+		isUnmanagedVPC bool
+		expect         func(m *mock_ec2iface.MockEC2APIMockRecorder)
+		wantErr        bool
+	}{
+		{
+			name:           "Should skip deletion if vpc is unmanaged",
+			isUnmanagedVPC: true,
+		},
+		{
+			name: "Should skip deletion if no private subnet is present",
+			input: []infrav1.SubnetSpec{
+				{
+					ID:               "subnet-1",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.10.0/24",
+					IsPublic:         true,
+				},
+			},
+		},
+		{
+			name: "Should skip deletion if no public subnet is present",
+			input: []infrav1.SubnetSpec{
+				{
+					ID:               "subnet-1",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.10.0/24",
+					IsPublic:         false,
+				},
+			},
+		},
+		{
+			name: "Should skip deletion if no existing natgateway present",
+			input: []infrav1.SubnetSpec{
+				{
+					ID:               "subnet-1",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.10.0/24",
+					IsPublic:         true,
+				},
+				{
+					ID:               "subnet-2",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.12.0/24",
+					IsPublic:         false,
+				},
+			},
+			expect: func(m *mock_ec2iface.MockEC2APIMockRecorder) {
+				m.DescribeNatGatewaysPages(
+					gomock.Eq(&ec2.DescribeNatGatewaysInput{
+						Filter: []*ec2.Filter{
+							{
+								Name:   aws.String("vpc-id"),
+								Values: []*string{aws.String("managed-vpc")},
+							},
+							{
+								Name:   aws.String("state"),
+								Values: []*string{aws.String("pending"), aws.String("available")},
+							},
+						},
+					}),
+					gomock.Any()).Return(nil)
+			},
+		},
+		{
+			name: "Should successfully delete natgateways",
+			input: []infrav1.SubnetSpec{
+				{
+					ID:               "subnet-1",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.10.0/24",
+					IsPublic:         true,
+				},
+				{
+					ID:               "subnet-2",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.12.0/24",
+					IsPublic:         false,
+				},
+			},
+			expect: func(m *mock_ec2iface.MockEC2APIMockRecorder) {
+				m.DescribeNatGatewaysPages(
+					gomock.AssignableToTypeOf(&ec2.DescribeNatGatewaysInput{}),
+					gomock.Any()).Do(mockDescribeNatGatewaysOutput).Return(nil)
+
+				m.DeleteNatGateway(gomock.Eq(&ec2.DeleteNatGatewayInput{
+					NatGatewayId: aws.String("natgateway"),
+				})).Return(&ec2.DeleteNatGatewayOutput{}, nil)
+
+				m.DescribeNatGateways(gomock.Eq(&ec2.DescribeNatGatewaysInput{
+					NatGatewayIds: []*string{aws.String("natgateway")},
+				})).Return(&ec2.DescribeNatGatewaysOutput{
+					NatGateways: []*ec2.NatGateway{
+						{
+							State: aws.String("available"),
+						},
+					},
+				}, nil)
+				m.DescribeNatGateways(gomock.AssignableToTypeOf(&ec2.DescribeNatGatewaysInput{})).Return(&ec2.DescribeNatGatewaysOutput{
+					NatGateways: []*ec2.NatGateway{
+						{
+							State: aws.String("deleted"),
+						},
+					},
+				}, nil)
+			},
+		},
+		{
+			name: "Should return error if natgateway has unknown state",
+			input: []infrav1.SubnetSpec{
+				{
+					ID:               "subnet-1",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.10.0/24",
+					IsPublic:         true,
+				},
+				{
+					ID:               "",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.12.0/24",
+					IsPublic:         true,
+				},
+				{
+					ID:               "subnet-3",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.14.0/24",
+					IsPublic:         false,
+				},
+			},
+			expect: func(m *mock_ec2iface.MockEC2APIMockRecorder) {
+				m.DescribeNatGatewaysPages(
+					gomock.AssignableToTypeOf(&ec2.DescribeNatGatewaysInput{}), gomock.Any()).Do(mockDescribeNatGatewaysOutput).Return(nil)
+
+				m.DeleteNatGateway(gomock.Eq(&ec2.DeleteNatGatewayInput{
+					NatGatewayId: aws.String("natgateway"),
+				})).Return(&ec2.DeleteNatGatewayOutput{}, nil)
+
+				m.DescribeNatGateways(gomock.Eq(&ec2.DescribeNatGatewaysInput{
+					NatGatewayIds: []*string{aws.String("natgateway")},
+				})).Return(&ec2.DescribeNatGatewaysOutput{
+					NatGateways: []*ec2.NatGateway{
+						{
+							State: aws.String("unknown"),
+						},
+					},
+				}, nil)
+			},
+			wantErr: true,
+		},
+		{
+			name: "Should return error if describe natgateway output and error, both are nil",
+			input: []infrav1.SubnetSpec{
+				{
+					ID:               "subnet-1",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.10.0/24",
+					IsPublic:         true,
+				},
+				{
+					ID:               "subnet-2",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.14.0/24",
+					IsPublic:         false,
+				},
+			},
+			expect: func(m *mock_ec2iface.MockEC2APIMockRecorder) {
+				m.DescribeNatGatewaysPages(
+					gomock.AssignableToTypeOf(&ec2.DescribeNatGatewaysInput{}), gomock.Any()).Do(mockDescribeNatGatewaysOutput).Return(nil)
+
+				m.DeleteNatGateway(gomock.Eq(&ec2.DeleteNatGatewayInput{
+					NatGatewayId: aws.String("natgateway"),
+				})).Return(&ec2.DeleteNatGatewayOutput{}, nil)
+
+				m.DescribeNatGateways(gomock.Eq(&ec2.DescribeNatGatewaysInput{
+					NatGatewayIds: []*string{aws.String("natgateway")},
+				})).Return(nil, nil)
+			},
+			wantErr: true,
+		},
+		{
+			name: "Should return error if describe natgateway pages fails",
+			input: []infrav1.SubnetSpec{
+				{
+					ID:               "subnet-1",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.10.0/24",
+					IsPublic:         true,
+				},
+				{
+					ID:               "subnet-2",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.14.0/24",
+					IsPublic:         false,
+				},
+			},
+			expect: func(m *mock_ec2iface.MockEC2APIMockRecorder) {
+				m.DescribeNatGatewaysPages(
+					gomock.AssignableToTypeOf(&ec2.DescribeNatGatewaysInput{}), gomock.Any()).Return(awserrors.NewFailedDependency("failed dependency"))
+			},
+			wantErr: true,
+		},
+		{
+			name: "Should return error if delete natgateway fails",
+			input: []infrav1.SubnetSpec{
+				{
+					ID:               "subnet-1",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.10.0/24",
+					IsPublic:         true,
+				},
+				{
+					ID:               "subnet-2",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.14.0/24",
+					IsPublic:         false,
+				},
+			},
+			expect: func(m *mock_ec2iface.MockEC2APIMockRecorder) {
+				m.DescribeNatGatewaysPages(
+					gomock.AssignableToTypeOf(&ec2.DescribeNatGatewaysInput{}), gomock.Any()).Do(mockDescribeNatGatewaysOutput).Return(nil)
+
+				m.DeleteNatGateway(gomock.Eq(&ec2.DeleteNatGatewayInput{
+					NatGatewayId: aws.String("natgateway"),
+				})).Return(nil, awserrors.NewFailedDependency("failed dependency"))
+			},
+			wantErr: true,
+		},
+		{
+			name: "Should return error if describe natgateway fails",
+			input: []infrav1.SubnetSpec{
+				{
+					ID:               "subnet-1",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.10.0/24",
+					IsPublic:         true,
+				},
+				{
+					ID:               "subnet-2",
+					AvailabilityZone: "us-east-1a",
+					CidrBlock:        "10.0.14.0/24",
+					IsPublic:         false,
+				},
+			},
+			expect: func(m *mock_ec2iface.MockEC2APIMockRecorder) {
+				m.DescribeNatGatewaysPages(
+					gomock.AssignableToTypeOf(&ec2.DescribeNatGatewaysInput{}), gomock.Any()).Do(mockDescribeNatGatewaysOutput).Return(nil)
+
+				m.DeleteNatGateway(gomock.Eq(&ec2.DeleteNatGatewayInput{
+					NatGatewayId: aws.String("natgateway"),
+				})).Return(&ec2.DeleteNatGatewayOutput{}, nil)
+
+				m.DescribeNatGateways(gomock.Eq(&ec2.DescribeNatGatewaysInput{
+					NatGatewayIds: []*string{aws.String("natgateway")},
+				})).Return(nil, awserrors.NewNotFound("not found"))
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewWithT(t)
+			ec2Mock := mock_ec2iface.NewMockEC2API(mockCtrl)
+			scheme := runtime.NewScheme()
+			_ = infrav1.AddToScheme(scheme)
+			awsCluster := &infrav1.AWSCluster{
+				ObjectMeta: metav1.ObjectMeta{Name: "test"},
+				Spec: infrav1.AWSClusterSpec{
+					NetworkSpec: infrav1.NetworkSpec{
+						VPC: infrav1.VPCSpec{
+							ID: "managed-vpc",
+							Tags: infrav1.Tags{
+								infrav1.ClusterTagKey("test-cluster"): "owned",
+							},
+						},
+						Subnets: tc.input,
+					},
+				},
+			}
+			if tc.isUnmanagedVPC {
+				awsCluster.Spec.NetworkSpec.VPC.Tags = infrav1.Tags{}
+			}
+			client := fake.NewClientBuilder().WithScheme(scheme).Build()
+			clusterScope, err := scope.NewClusterScope(scope.ClusterScopeParams{
+				Cluster: &clusterv1.Cluster{
+					ObjectMeta: metav1.ObjectMeta{Name: "test-cluster"},
+				},
+				AWSCluster: awsCluster,
+				Client:     client,
+			})
+			g.Expect(err).NotTo(HaveOccurred())
+			if tc.expect != nil {
+				tc.expect(ec2Mock.EXPECT())
+			}
+
+			s := NewService(clusterScope)
+			s.EC2Client = ec2Mock
+
+			err = s.deleteNatGateways()
+			if tc.wantErr {
+				g.Expect(err).To(HaveOccurred())
+				return
+			}
+			g.Expect(err).NotTo(HaveOccurred())
+		})
+	}
+}
+
+var mockDescribeNatGatewaysOutput = func(_, y interface{}) {
+	funct := y.(func(page *ec2.DescribeNatGatewaysOutput, lastPage bool) bool)
+	funct(&ec2.DescribeNatGatewaysOutput{NatGateways: []*ec2.NatGateway{{
+		NatGatewayId: aws.String("natgateway"),
+		SubnetId:     aws.String("subnet-1"),
+	}}}, true)
 }
