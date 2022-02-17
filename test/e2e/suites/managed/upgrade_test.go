42918e2b83af2452c0d01ba0cc78c6f60cf72f73
Winnie Kwon <wkwon@vmware.com>
2022-02-17 12:55:41 -0800

    Enable EKS upgrade test
    Update k8s version to EKS supported versions.
    Set up AWSClusterStaticIdentity for the managed test suite.
    
    A few refactoring of the code.
    - Not to specify namespace in AWSClusterStaticIdentity as it is a
    cluster-scoped object.
    - Delete DeleteClusterSpec helper func that could be replaced with CAPI
    framework function.

--- test/e2e/suites/managed/upgrade_test.go
+++ test/e2e/suites/managed/upgrade_test.go
@@ -23,20 +23,21 @@ import (
 	"context"
 	"fmt"
 
-	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 
+	ekscontrolplanev1 "sigs.k8s.io/cluster-api-provider-aws/controlplane/eks/api/v1beta1"
 	"sigs.k8s.io/cluster-api-provider-aws/test/e2e/shared"
 	"sigs.k8s.io/cluster-api/test/framework"
 	"sigs.k8s.io/cluster-api/util"
 )
 
 // EKS cluster upgrade tests.
-var _ = Describe("EKS Cluster upgrade test", func() {
+var _ = ginkgo.Describe("EKS Cluster upgrade test", func() {
 	const (
-		initialVersion   = "v1.17.0"
-		upgradeToversion = "v1.18.0"
+		initialVersion   = "v1.20.0"
+		upgradeToversion = "v1.21.0"
 	)
 	var (
 		namespace   *corev1.Namespace
@@ -46,7 +47,7 @@ var _ = Describe("EKS Cluster upgrade test", func() {
 	)
 
 	shared.ConditionalIt(runUpgradeTests, "[managed] [upgrade] should create a cluster and upgrade the kubernetes version", func() {
-		By("should have a valid test configuration")
+		ginkgo.By("should have a valid test configuration")
 		Expect(e2eCtx.Environment.BootstrapClusterProxy).ToNot(BeNil(), "Invalid argument. BootstrapClusterProxy can't be nil")
 		Expect(e2eCtx.E2EConfig).ToNot(BeNil(), "Invalid argument. e2eConfig can't be nil when calling %s spec", specName)
 		Expect(e2eCtx.E2EConfig.Variables).To(HaveKey(shared.KubernetesVersion))
@@ -55,7 +56,10 @@ var _ = Describe("EKS Cluster upgrade test", func() {
 		namespace = shared.SetupSpecNamespace(ctx, specName, e2eCtx)
 		clusterName = fmt.Sprintf("cluster-%s", util.RandomString(6))
 
-		By("should create an EKS control plane")
+		ginkgo.By("default iam role should exist")
+		verifyRoleExistsAndOwned(ekscontrolplanev1.DefaultEKSControlPlaneRole, clusterName, false, e2eCtx.BootstrapUserAWSSession)
+
+		ginkgo.By("should create an EKS control plane")
 		ManagedClusterSpec(ctx, func() ManagedClusterSpecInput {
 			return ManagedClusterSpecInput{
 				E2EConfig:                e2eCtx.E2EConfig,
@@ -104,16 +108,13 @@ var _ = Describe("EKS Cluster upgrade test", func() {
 
 		// TODO (richardcase): add test for the node group upgrade
 
-		shared.Byf("should delete cluster %s", clusterName)
-		DeleteClusterSpec(ctx, func() DeleteClusterSpecInput {
-			return DeleteClusterSpecInput{
-				E2EConfig:             e2eCtx.E2EConfig,
-				BootstrapClusterProxy: e2eCtx.Environment.BootstrapClusterProxy,
-				ClusterName:           clusterName,
-				Namespace:             namespace,
-			}
+		framework.DeleteCluster(ctx, framework.DeleteClusterInput{
+			Deleter: e2eCtx.Environment.BootstrapClusterProxy.GetClient(),
+			Cluster: cluster,
 		})
-
+		framework.WaitForClusterDeleted(ctx, framework.WaitForClusterDeletedInput{
+			Getter:  e2eCtx.Environment.BootstrapClusterProxy.GetClient(),
+			Cluster: cluster,
+		}, e2eCtx.E2EConfig.GetIntervals("", "wait-delete-cluster")...)
 	})
-
 })
