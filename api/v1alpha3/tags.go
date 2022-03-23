59d61592af152974f043cd981dbf7f6647c0ed19
Vibhor Chinda <vibhorchinda@gmail.com>
2022-03-23 14:19:24 +0530

    removed equals function in tag.go

--- api/v1alpha3/tags.go
+++ api/v1alpha3/tags.go
@@ -19,7 +19,6 @@ package v1alpha3
 import (
 	"fmt"
 
-	"github.com/google/go-cmp/cmp"
 	"k8s.io/apimachinery/pkg/types"
 
 	clusterv1alpha3 "sigs.k8s.io/cluster-api/api/v1alpha3"
@@ -27,11 +26,6 @@ import (
 
 // Tags defines a map of tags.
 type Tags map[string]string
-
-// Equals returns true if the tags are equal.
-func (t Tags) Equals(other Tags) bool {
-	return cmp.Equal(t, other)
-}
 
 // HasOwned returns true if the tags contains a tag that marks the resource as owned by the cluster from the perspective of this management tooling.
 func (t Tags) HasOwned(cluster string) bool {
