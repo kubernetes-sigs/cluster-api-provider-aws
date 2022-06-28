744278344d4ebc3d801bbd10dd03373386bccd6e
Richard Case <richard.case@outlook.com>
2022-06-28 12:49:46 +0100

    feat: added ability to enable/disable gc for a cluster
    As part of review ot was mentioned that it would be good to have a way
    to manually disable garbage collection for a cluster. This would be
    especially useful if access to the workload/tenant cluster had been
    lost.
    
    This change introduces commands to `clusterawsadm` that enables you to
    manually enable and disable garbage collection for a specific cluster.
    
    The docs have been updated with instructions.
    
    Signed-off-by: Richard Case <richard.case@outlook.com>

---
+++ cmd/clusterawsadm/cmd/gc/disable.go
@@ -0,0 +1,83 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gc
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/util/homedir"
+
+	gcproc "sigs.k8s.io/cluster-api-provider-aws/cmd/clusterawsadm/gc"
+	"sigs.k8s.io/cluster-api/cmd/clusterctl/cmd"
+)
+
+func newDisableCmd() *cobra.Command {
+	clusterName := ""
+	namespace := ""
+	kubeConfig := ""
+	kubeConfigDefault := ""
+
+	if home := homedir.HomeDir(); home != "" {
+		kubeConfigDefault = filepath.Join(home, ".kube", "config")
+	}
+
+	newCmd := &cobra.Command{
+		Use:   "disable",
+		Short: "Mark a cluster as NOT requiring external resource gc",
+		Long: cmd.LongDesc(`
+			This command will mark the given cluster as not requiring external
+			resource garbage collection (i.e. deleting) when the cluster is
+			requested to be deleted.
+		`),
+		Example: cmd.Examples(`
+			# Disable GC for a cluster using existing k8s context
+			clusterawsadm gc disable --cluster-name=test-cluster
+
+			# Disable GC for a cluster using kubeconfig
+			clusterawsadm gc disable --cluster-name=test-cluster --kubeconfig=test.kubeconfig
+		`),
+		Args: cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			proc, err := gcproc.New(gcproc.GCInput{
+				ClusterName:    clusterName,
+				Namespace:      namespace,
+				KubeconfigPath: kubeConfig,
+			})
+			if err != nil {
+				return fmt.Errorf("creating command processor: %w", err)
+			}
+
+			err = proc.Disable(cmd.Context())
+			if err != nil {
+				return fmt.Errorf("disabling garbage collection: %w", err)
+			}
+			fmt.Printf("Disabled garbage collection for cluster %s/%s\n", namespace, clusterName)
+
+			return nil
+		},
+	}
+
+	newCmd.Flags().StringVar(&clusterName, "cluster-name", "", "The name of the CAPA cluster")
+	newCmd.Flags().StringVarP(&namespace, "namespace", "n", "default", "The namespace for the cluster definition")
+	newCmd.Flags().StringVar(&kubeConfig, "kubeconfig", kubeConfigDefault, "Path to the kubeconfig file to use")
+
+	newCmd.MarkFlagRequired("cluster-name") //nolint: errcheck
+
+	return newCmd
+}
---
+++ cmd/clusterawsadm/gc/gc_test.go
@@ -0,0 +1,282 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gc
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	infrav1 "sigs.k8s.io/cluster-api-provider-aws/api/v1beta1"
+	ekscontrolplanev1 "sigs.k8s.io/cluster-api-provider-aws/controlplane/eks/api/v1beta1"
+	"sigs.k8s.io/cluster-api-provider-aws/pkg/annotations"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	"sigs.k8s.io/cluster-api/controllers/external"
+)
+
+func TestEnableGC(t *testing.T) {
+	RegisterTestingT(t)
+
+	testClusterName := "test-cluster"
+
+	testCases := []struct {
+		name         string
+		clusterName  string
+		existingObjs []client.Object
+		expectError  bool
+	}{
+		{
+			name:         "no capi cluster",
+			clusterName:  testClusterName,
+			existingObjs: []client.Object{},
+			expectError:  true,
+		},
+		{
+			name:         "no infra cluster",
+			clusterName:  testClusterName,
+			existingObjs: newManagedCluster(testClusterName, true),
+			expectError:  true,
+		},
+		{
+			name:         "with managed control plane and no annotation",
+			clusterName:  testClusterName,
+			existingObjs: newManagedCluster(testClusterName, false),
+			expectError:  false,
+		},
+		{
+			name:         "with awscluster and no annotation",
+			clusterName:  testClusterName,
+			existingObjs: newUnManagedCluster(testClusterName, false),
+			expectError:  false,
+		},
+		{
+			name:         "with managed control plane and existing annotation",
+			clusterName:  testClusterName,
+			existingObjs: newManagedClusterWithAnnotations(testClusterName, map[string]string{annotations.ExternalResourceGCAnnotation: "true"}),
+			expectError:  false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewWithT(t)
+
+			input := GCInput{
+				ClusterName: tc.clusterName,
+				Namespace:   "default",
+			}
+
+			fake := newFakeClient(scheme, tc.existingObjs...)
+			ctx := context.TODO()
+
+			proc, err := New(input, WithClient(fake))
+			g.Expect(err).NotTo(HaveOccurred())
+
+			resErr := proc.Enable(ctx)
+			if tc.expectError {
+				g.Expect(resErr).To(HaveOccurred())
+				return
+			}
+			g.Expect(resErr).NotTo(HaveOccurred())
+
+			cluster := tc.existingObjs[0].(*clusterv1.Cluster)
+			ref := cluster.Spec.InfrastructureRef
+
+			obj, err := external.Get(ctx, fake, ref, "default")
+			g.Expect(err).NotTo(HaveOccurred())
+			g.Expect(obj).NotTo(BeNil())
+
+			gcVal, found := annotations.Get(obj, annotations.ExternalResourceGCAnnotation)
+			g.Expect(found).To(BeTrue())
+			g.Expect(gcVal).To(Equal("false"))
+		})
+	}
+}
+
+func TestDisableGC(t *testing.T) {
+	RegisterTestingT(t)
+
+	testClusterName := "test-cluster"
+
+	testCases := []struct {
+		name         string
+		clusterName  string
+		existingObjs []client.Object
+		expectError  bool
+	}{
+		{
+			name:         "no capi cluster",
+			clusterName:  testClusterName,
+			existingObjs: []client.Object{},
+			expectError:  true,
+		},
+		{
+			name:         "no infra cluster",
+			clusterName:  testClusterName,
+			existingObjs: newManagedCluster(testClusterName, true),
+			expectError:  true,
+		},
+		{
+			name:         "with managed control plane and no annotation",
+			clusterName:  testClusterName,
+			existingObjs: newManagedCluster(testClusterName, false),
+			expectError:  false,
+		},
+		{
+			name:         "with awscluster and no annotation",
+			clusterName:  testClusterName,
+			existingObjs: newUnManagedCluster(testClusterName, false),
+			expectError:  false,
+		},
+		{
+			name:         "with managed control plane and existing annotation",
+			clusterName:  testClusterName,
+			existingObjs: newManagedClusterWithAnnotations(testClusterName, map[string]string{annotations.ExternalResourceGCAnnotation: "false"}),
+			expectError:  false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewWithT(t)
+
+			input := GCInput{
+				ClusterName: tc.clusterName,
+				Namespace:   "default",
+			}
+
+			fake := newFakeClient(scheme, tc.existingObjs...)
+			ctx := context.TODO()
+
+			proc, err := New(input, WithClient(fake))
+			g.Expect(err).NotTo(HaveOccurred())
+
+			resErr := proc.Disable(ctx)
+			if tc.expectError {
+				g.Expect(resErr).To(HaveOccurred())
+				return
+			}
+			g.Expect(resErr).NotTo(HaveOccurred())
+
+			cluster := tc.existingObjs[0].(*clusterv1.Cluster)
+			ref := cluster.Spec.InfrastructureRef
+
+			obj, err := external.Get(ctx, fake, ref, "default")
+			g.Expect(err).NotTo(HaveOccurred())
+			g.Expect(obj).NotTo(BeNil())
+
+			gcVal, found := annotations.Get(obj, annotations.ExternalResourceGCAnnotation)
+			g.Expect(found).To(BeTrue())
+			g.Expect(gcVal).To(Equal("true"))
+		})
+	}
+}
+
+func newFakeClient(scheme *runtime.Scheme, objs ...client.Object) client.Client {
+	return fake.NewClientBuilder().WithScheme(scheme).WithObjects(objs...).Build()
+}
+
+func newManagedCluster(name string, excludeInfra bool) []client.Object {
+	objs := []client.Object{
+		&clusterv1.Cluster{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Cluster",
+				APIVersion: clusterv1.GroupVersion.String(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: "default",
+			},
+			Spec: clusterv1.ClusterSpec{
+				InfrastructureRef: &corev1.ObjectReference{
+					Name:       name,
+					Namespace:  "default",
+					Kind:       "AWSManagedControlPlane",
+					APIVersion: ekscontrolplanev1.GroupVersion.String(),
+				},
+			},
+		},
+	}
+
+	if !excludeInfra {
+		objs = append(objs, &ekscontrolplanev1.AWSManagedControlPlane{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "AWSManagedControlPlane",
+				APIVersion: ekscontrolplanev1.GroupVersion.String(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: "default",
+			},
+		})
+	}
+
+	return objs
+}
+
+func newManagedClusterWithAnnotations(name string, annotations map[string]string) []client.Object {
+	objs := newManagedCluster(name, false)
+
+	mcp := objs[1].(*ekscontrolplanev1.AWSManagedControlPlane)
+	mcp.ObjectMeta.Annotations = annotations
+
+	return objs
+}
+
+func newUnManagedCluster(name string, excludeInfra bool) []client.Object {
+	objs := []client.Object{
+		&clusterv1.Cluster{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Cluster",
+				APIVersion: clusterv1.GroupVersion.String(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: "default",
+			},
+			Spec: clusterv1.ClusterSpec{
+				InfrastructureRef: &corev1.ObjectReference{
+					Name:       name,
+					Namespace:  "default",
+					Kind:       "AWSCluster",
+					APIVersion: infrav1.GroupVersion.String(),
+				},
+			},
+		},
+	}
+
+	if !excludeInfra {
+		objs = append(objs, &infrav1.AWSCluster{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "AWSCluster",
+				APIVersion: infrav1.GroupVersion.String(),
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: "default",
+			},
+		})
+	}
+
+	return objs
+}
