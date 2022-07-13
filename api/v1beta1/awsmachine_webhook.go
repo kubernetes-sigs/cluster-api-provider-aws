3fedadaa47825e66fffd953c2de910b23a1549fb
Amit Sahastrabuddhe <amit.sahastra@gmail.com>
2022-07-13 19:00:08 +0530

    Fix BET-4653

--- api/v1beta1/awsmachine_webhook.go
+++ api/v1beta1/awsmachine_webhook.go
@@ -114,14 +114,6 @@ func (r *AWSMachine) ValidateUpdate(old runtime.Object) error {
 		delete(cloudInit, "secureSecretsBackend")
 	}
 
-	if rootVolume, ok := oldAWSMachineSpec["rootVolume"].(map[string]interface{}); ok {
-		delete(rootVolume, "deviceName")
-	}
-
-	if rootVolume, ok := newAWSMachineSpec["rootVolume"].(map[string]interface{}); ok {
-		delete(rootVolume, "deviceName")
-	}
-
 	if !cmp.Equal(oldAWSMachineSpec, newAWSMachineSpec) {
 		s := fmt.Sprintf("oldAWSMachineSpec: %s, newAWSMachineSpec: %s do not match", oldAWSMachineSpec, newAWSMachineSpec)
 		allErrs = append(allErrs, field.Forbidden(field.NewPath("spec"), "cannot be modified."+s))
