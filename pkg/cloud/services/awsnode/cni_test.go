8af61b4f6e867b5cff5f4c280d1b0ee464aeef34
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-08-19 10:02:34 +0200

    Fix the update logic for aws-node daemonset environment properties

--- pkg/cloud/services/awsnode/cni_test.go
+++ pkg/cloud/services/awsnode/cni_test.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/gomega"
 	v1 "k8s.io/api/apps/v1"
@@ -27,6 +28,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	infrav1 "sigs.k8s.io/cluster-api-provider-aws/api/v1beta1"
 	ekscontrolplanev1 "sigs.k8s.io/cluster-api-provider-aws/controlplane/eks/api/v1beta1"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/scope"
 )
@@ -206,6 +208,92 @@ func TestReconcileCniVpcCniValues(t *testing.T) {
 	}
 }
 
+func TestReconcileCniVpcCniValuesWithSecondaryCidrBlock(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	g := NewWithT(t)
+	daemonSet := &v1.DaemonSet{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "DaemonSet",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "aws-node",
+			Namespace: "kube-system",
+		},
+		Spec: v1.DaemonSetSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name: "aws-node",
+							Env: []corev1.EnvVar{
+								{
+									Name:  "NAME1",
+									Value: "OVERWRITE",
+								},
+								{
+									Name:  "NAME3",
+									Value: "VALUE3",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	values := ekscontrolplanev1.VpcCni{
+		Env: []corev1.EnvVar{
+			{
+				Name:  "NAME1",
+				Value: "VALUE1",
+			},
+		},
+	}
+	mockClient := &cachingClient{
+		getValue: daemonSet,
+	}
+	m := &mockScope{
+		client:             mockClient,
+		cni:                values,
+		secondaryCidrBlock: aws.String("100.0.0.1/20"),
+		securityGroups: map[infrav1.SecurityGroupRole]infrav1.SecurityGroup{
+			"node": {
+				ID:   "sg-1234",
+				Name: "node",
+			},
+		},
+		subnets: []infrav1.SubnetSpec{},
+	}
+	s := NewService(m)
+
+	err := s.ReconcileCNI(context.Background())
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(mockClient.updateChain).NotTo(BeEmpty())
+	ds, ok := mockClient.updateChain[0].(*v1.DaemonSet)
+	g.Expect(ok).To(BeTrue())
+	g.Expect(ds.Spec.Template.Spec.Containers).NotTo(BeEmpty())
+	g.Expect(ds.Spec.Template.Spec.Containers[0].Env).To(ConsistOf([]corev1.EnvVar{
+		{
+			Name:  "NAME1",
+			Value: "VALUE1",
+		},
+		{
+			Name:  "NAME3",
+			Value: "VALUE3",
+		},
+		{
+			Name:  "AWS_VPC_K8S_CNI_CUSTOM_NETWORK_CFG",
+			Value: "true",
+		},
+		{
+			Name:  "ENI_CONFIG_LABEL_DEF",
+			Value: "failure-domain.beta.kubernetes.io/zone",
+		},
+	}))
+}
+
 type cachingClient struct {
 	client.Client
 	getValue    client.Object
@@ -225,10 +313,17 @@ func (c *cachingClient) Update(ctx context.Context, obj client.Object, opts ...c
 	return nil
 }
 
+func (c *cachingClient) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
+	return nil
+}
+
 type mockScope struct {
 	scope.AWSNodeScope
-	client client.Client
-	cni    ekscontrolplanev1.VpcCni
+	client             client.Client
+	cni                ekscontrolplanev1.VpcCni
+	secondaryCidrBlock *string
+	securityGroups     map[infrav1.SecurityGroupRole]infrav1.SecurityGroup
+	subnets            infrav1.Subnets
 }
 
 func (s *mockScope) RemoteClient() (client.Client, error) {
@@ -256,5 +351,13 @@ func (s *mockScope) DisableVPCCNI() bool {
 }
 
 func (s *mockScope) SecondaryCidrBlock() *string {
-	return nil
+	return s.secondaryCidrBlock
+}
+
+func (s *mockScope) SecurityGroups() map[infrav1.SecurityGroupRole]infrav1.SecurityGroup {
+	return s.securityGroups
+}
+
+func (s *mockScope) Subnets() infrav1.Subnets {
+	return s.subnets
 }
