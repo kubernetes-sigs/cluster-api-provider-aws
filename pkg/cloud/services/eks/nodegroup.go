f869ba584c58dd94577d9e023ed27a56cfc79d7f
Snehal Amrutkar <snehal@spectrocloud.com>
2022-11-23 22:25:44 +0530

    EKS: minSize and maxSize updates

--- pkg/cloud/services/eks/nodegroup.go
+++ pkg/cloud/services/eks/nodegroup.go
@@ -486,6 +486,10 @@ func (s *NodegroupService) reconcileNodegroupConfig(ng *eks.Nodegroup) error {
 		(aws.Int64Value(ng.ScalingConfig.MinSize) != int64(aws.Int32Value(managedPool.Scaling.MinSize)))) {
 		s.Debug("Nodegroup min/max differ from spec, updating scaling configuration", "nodegroup", ng.NodegroupName)
 		input.ScalingConfig = s.scalingConfig()
+		if *ng.ScalingConfig.DesiredSize < int64(aws.Int32Value(managedPool.Scaling.MinSize)) {
+			desiredSize := int64(aws.Int32Value(managedPool.Scaling.MinSize))
+			input.ScalingConfig.DesiredSize = &desiredSize
+		}
 		needsUpdate = true
 	}
 	currentUpdateConfig := converters.NodegroupUpdateconfigFromSDK(ng.UpdateConfig)
