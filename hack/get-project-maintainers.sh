4725d3c584be47f60a9b9d05e1a8a672a1f4b48d
Richard Case <richard.case@outlook.com>
2023-03-21 14:29:19 +0000

    feat(release): update image promotion to use kpromo
    The release process has been updated to use `kpromo` for handling
    the image promotion instead of the manual process we had.
    
    Signed-off-by: Richard Case <richard.case@outlook.com>

--- hack/get-project-maintainers.sh
+++ hack/get-project-maintainers.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Copyright 2021 The Kubernetes Authors.
+
+# Copyright 2023 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,29 +18,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
 if [ -z "${1}" ]; then
-  echo "must provide module as first parameter"
+  echo "must provide path to yq as first parameter"
   exit 1
 fi
 
-if [ -z "${2}" ]; then
-  echo "must provide binary name as second parameter"
-  exit 1
-fi
-
-if [ -z "${3}" ]; then
-  echo "must provide version as third parameter"
-  exit 1
-fi
-
-if [ -z "${GOBIN}" ]; then
-  echo "GOBIN is not set. Must set GOBIN to install the bin in a specified directory."
-  exit 1
-fi
-
-rm "${GOBIN}/${2}"* || true
-
-# install the golang module specified as the first argument
-go install "${1}@${3}"
-mv "${GOBIN}/${2}" "${GOBIN}/${2}-${3}"
-ln -sf "${GOBIN}/${2}-${3}" "${GOBIN}/${2}"
+KEYS=()
+while IFS='' read -r line; do KEYS+=("$line"); done < <(${1} e '.aliases["cluster-api-aws-maintainers"][]' OWNERS_ALIASES)
+echo "${KEYS[@]/#/@}"
