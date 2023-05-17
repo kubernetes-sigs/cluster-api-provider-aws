26ef70ac7cab8b9ace2196ffcbf934a2f8d2bc42
Vince Prignano <vincepri@redhat.com>
2023-05-17 08:09:29 -0700

    :sparkles: Add support to specify PlacementGroup Name in instances
    Signed-off-by: Vince Prignano <vincepri@redhat.com>

--- api/v1beta1/awsmachine_conversion.go
+++ api/v1beta1/awsmachine_conversion.go
@@ -37,6 +37,7 @@ func (src *AWSMachine) ConvertTo(dstRaw conversion.Hub) error {
 
 	dst.Spec.Ignition = restored.Spec.Ignition
 	dst.Spec.InstanceMetadataOptions = restored.Spec.InstanceMetadataOptions
+	dst.Spec.PlacementGroupName = restored.Spec.PlacementGroupName
 
 	return nil
 }
@@ -83,6 +84,7 @@ func (r *AWSMachineTemplate) ConvertTo(dstRaw conversion.Hub) error {
 	dst.Spec.Template.ObjectMeta = restored.Spec.Template.ObjectMeta
 	dst.Spec.Template.Spec.Ignition = restored.Spec.Template.Spec.Ignition
 	dst.Spec.Template.Spec.InstanceMetadataOptions = restored.Spec.Template.Spec.InstanceMetadataOptions
+	dst.Spec.Template.Spec.PlacementGroupName = restored.Spec.Template.Spec.PlacementGroupName
 
 	return nil
 }
--- api/v1beta2/types.go
+++ api/v1beta2/types.go
@@ -194,6 +194,10 @@ type Instance struct {
 	// SpotMarketOptions option for configuring instances to be run using AWS Spot instances.
 	SpotMarketOptions *SpotMarketOptions `json:"spotMarketOptions,omitempty"`
 
+	// PlacementGroupName specifies the name of the placement group in which to launch the instance.
+	// +optional
+	PlacementGroupName string `json:"placementGroupName,omitempty"`
+
 	// Tenancy indicates if instance should run on shared or single-tenant hardware.
 	// +optional
 	Tenancy string `json:"tenancy,omitempty"`
