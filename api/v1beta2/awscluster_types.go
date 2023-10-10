bac5088f24d246b1b0fa3fc243d1e391ff96116c
Vince Prignano <vincepri@redhat.com>
2023-10-10 07:53:44 -0700

    :sparkles: Support additional TCP listeners on NLBs
    Signed-off-by: Vince Prignano <vincepri@redhat.com>

--- api/v1beta2/awscluster_types.go
+++ api/v1beta2/awscluster_types.go
@@ -208,6 +208,13 @@ type AWSLoadBalancerSpec struct {
 	// +optional
 	AdditionalSecurityGroups []string `json:"additionalSecurityGroups,omitempty"`
 
+	// AdditionalListeners sets the additional listeners for the control plane load balancer.
+	// This is only applicable to Network Load Balancer (NLB) types for the time being.
+	// +listType=map
+	// +listMapKey=port
+	// +optional
+	AdditionalListeners []AdditionalListenerSpec `json:"additionalListeners,omitempty"`
+
 	// IngressRules sets the ingress rules for the control plane load balancer.
 	// +optional
 	IngressRules []IngressRule `json:"ingressRules,omitempty"`
@@ -224,6 +231,20 @@ type AWSLoadBalancerSpec struct {
 	// PreserveClientIP lets the user control if preservation of client ips must be retained or not.
 	// If this is enabled 6443 will be opened to 0.0.0.0/0.
 	PreserveClientIP bool `json:"preserveClientIP,omitempty"`
+}
+
+// AdditionalListenerSpec defines the desired state of an
+// additional listener on an AWS load balancer.
+type AdditionalListenerSpec struct {
+	// Port sets the port for the additional listener.
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=65535
+	Port int64 `json:"port"`
+	// Protocol sets the protocol for the additional listener.
+	// Currently only TCP is supported.
+	// +kubebuilder:validation:Enum=TCP
+	// +kubebuilder:default=TCP
+	Protocol ELBProtocol `json:"protocol,omitempty"`
 }
 
 // AWSClusterStatus defines the observed state of AWSCluster.
