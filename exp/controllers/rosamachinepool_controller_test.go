0e0e4741d3572505342cd2e2acf461aa9ee6a6ea
Mulham Raee <mraee@redhat.com>
2024-03-14 13:33:05 +0100

    add additionalTags API fied to ROSAMachinePool
    - use the correct typemetav1.Duration for NodeDrainGracePeriod field
    - fixed reconcileProviderIDList() listing terminated/stopped instances

--- exp/controllers/rosamachinepool_controller_test.go
+++ exp/controllers/rosamachinepool_controller_test.go
@@ -2,10 +2,13 @@ package controllers
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
 
+	infrav1 "sigs.k8s.io/cluster-api-provider-aws/v2/api/v1beta2"
 	expinfrav1 "sigs.k8s.io/cluster-api-provider-aws/v2/exp/api/v1beta2"
 	expclusterv1 "sigs.k8s.io/cluster-api/exp/api/v1beta1"
 )
@@ -20,6 +23,12 @@ func TestNodePoolToRosaMachinePoolSpec(t *testing.T) {
 		AutoRepair:    true,
 		InstanceType:  "m5.large",
 		TuningConfigs: []string{"config1"},
+		NodeDrainGracePeriod: &metav1.Duration{
+			Duration: time.Minute * 10,
+		},
+		AdditionalTags: infrav1.Tags{
+			"tag1": "value1",
+		},
 	}
 
 	machinePoolSpec := expclusterv1.MachinePoolSpec{
