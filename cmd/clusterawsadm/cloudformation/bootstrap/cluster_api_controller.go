a7309944030965c1333bcc8d7bb97e790fc47d15
toby-archer-tr <149683518+toby-archer-tr@users.noreply.github.com>
2025-01-23 11:13:14 +0100

    Update cluster_api_controller.go

--- cmd/clusterawsadm/cloudformation/bootstrap/cluster_api_controller.go
+++ cmd/clusterawsadm/cloudformation/bootstrap/cluster_api_controller.go
@@ -418,8 +418,8 @@ func (t Template) ControllersPolicyEKS() *iamv1.PolicyDocument {
 				"arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
 			        "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
 				"arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-			}
-			Effect: iamv1.EffectAllow
+			},
+			Effect: iamv1.EffectAllow,
 		})
 	}
 
