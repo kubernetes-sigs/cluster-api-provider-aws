a19efa10977ed473c047b603235dd957b1501015
Toby Archer <toby.archer@traderepublic.com>
2025-02-07 16:12:30 +0100

    Fix managedmachinepool additional roles

--- cmd/clusterawsadm/cloudformation/bootstrap/cluster_api_controller.go
+++ cmd/clusterawsadm/cloudformation/bootstrap/cluster_api_controller.go
@@ -409,14 +409,14 @@ func (t Template) ControllersPolicyEKS() *iamv1.PolicyDocument {
 		})
 	}
 
-	if !t.Spec.EKS.ManagedMachinePools.Disabled {
+	if !t.Spec.EKS.ManagedMachinePool.Disable {
 		statements = append(statements, iamv1.StatementEntry{
 			Action: iamv1.Actions{
 				"iam:GetPolicy",
 			},
-			Resources: iamv1.Resources{
+			Resource: iamv1.Resources{
 				"arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-			        "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+				"arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
 				"arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
 			},
 			Effect: iamv1.EffectAllow,
