f28e4152cbd47fb3e7eb2c15a090d7464b596f47
Richard Case <richard.case@outlook.com>
2025-02-11 15:27:39 +0000

    feat: classic elb fix for TLS issues
    There is an issue when creating clusters (or upgrading clusters) with
    kubernetes versions v1.30+ and using a classic elb with an SSL health
    check (which the default for new clusters).
    
    The problem is that Kubernetes v1.30+ switched to Go 1.22 which removed
    the RSA ciphers. This then causes the ELB health check to fail.
    
    There are a number of workarounds including manually specifying the
    cipher suites to use for the api server.
    
    This commit does the following:
    
    - Adds warnings to the AWSCluster webhook to alert users that:
      - their cluster is using a classic elb and this is now deprecated
      - their cluster is using the default health check protocol which warnings
      previously SSL and that now the default has changed to TCP.
    - Will update the health check to TCP if the load balancer is "classic"
      and the health check protocol is not set.
    
    Signed-off-by: Richard Case <richard.case@outlook.com>

--- controllers/awsmachine_controller_unit_test.go
+++ controllers/awsmachine_controller_unit_test.go
@@ -2733,7 +2733,7 @@ func TestAWSMachineReconcilerReconcileDefaultsToLoadBalancerTypeClassic(t *testi
 	}, nil)
 
 	// Must attach to a classic LB, not another type. Only these mock calls are therefore expected.
-	mockedCreateLBCalls(t, elbMock.EXPECT())
+	mockedCreateLBCalls(t, elbMock.EXPECT(), false)
 
 	ec2Mock.EXPECT().DescribeNetworkInterfacesWithContext(context.TODO(), gomock.Eq(&ec2.DescribeNetworkInterfacesInput{Filters: []*ec2.Filter{
 		{
@@ -2750,7 +2750,8 @@ func TestAWSMachineReconcilerReconcileDefaultsToLoadBalancerTypeClassic(t *testi
 					},
 				},
 			},
-		}}, nil).MaxTimes(3)
+		},
+	}, nil).MaxTimes(3)
 	ec2Mock.EXPECT().DescribeNetworkInterfaceAttributeWithContext(context.TODO(), gomock.Eq(&ec2.DescribeNetworkInterfaceAttributeInput{
 		NetworkInterfaceId: aws.String("eni-1"),
 		Attribute:          aws.String("groupSet"),
