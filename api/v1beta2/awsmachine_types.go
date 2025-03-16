c9baa036e54e2258e0587d781bee96e631e09ec5
Thirumalesh Aaraveti <athiruma@redhat.com>
2025-03-16 23:46:14 +0530

    Add CEL validation

--- api/v1beta2/awsmachine_types.go
+++ api/v1beta2/awsmachine_types.go
@@ -220,6 +220,7 @@ type AWSMachineSpec struct {
 	//  If this value is selected, CapacityReservationID must be specified to identify the target reservation.
 	// If marketType is not specified and spotMarketOptions is provided, the marketType defaults to "Spot".
 	// +optional
+	// +kubebuilder:validation:XValidation:rule="!has(self.capacityReservationID) || !has(self.marketType) || self.marketType != 'Spot'",message="capacityReservationID may not be set when marketType is Spot"
 	MarketType MarketType `json:"marketType,omitempty"`
 }
 
