a7cb24b467beb7e05a8f4aee8e89e269a9338ad6
Nolan Brubaker <nolan@nbrubaker.com>
2025-05-12 13:08:20 -0400

    UPSTREAM: <carry>: Reference main branch for CI
    Signed-off-by: Nolan Brubaker <nolan@nbrubaker.com>

--- openshift/e2e-tests.sh
+++ openshift/e2e-tests.sh
@@ -7,12 +7,8 @@ echo "Running e2e-tests.sh"
 unset GOFLAGS
 tmp="$(mktemp -d)"
 
-if [ "${PULL_BASE_REF}" == "master" ]; then
-  # the default branch for cluster-capi-operator is main.
-  CCAPIO_BASE_REF="main"
-else
-  CCAPIO_BASE_REF=$PULL_BASE_REF
-fi
+# Default branch for both CCAPIO and this repo should be `main`.
+CCAPIO_BASE_REF=$PULL_BASE_REF
 
 echo "cloning github.com/openshift/cluster-capi-operator at branch '$CCAPIO_BASE_REF'"
 git clone --single-branch --branch="$CCAPIO_BASE_REF" --depth=1 "https://github.com/openshift/cluster-capi-operator.git" "$tmp"
