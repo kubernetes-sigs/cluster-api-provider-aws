3531eda5f5a85083dbe69e721867b053dd634d5c
Amit Sahastrabuddhe <amit.sahastra@gmail.com>
2025-06-27 09:27:44 +0530

    Code refactor

--- bootstrap/eks/controllers/eksconfig_controller.go
+++ bootstrap/eks/controllers/eksconfig_controller.go
@@ -252,6 +252,7 @@ func (r *EKSConfigReconciler) joinWorker(ctx context.Context, cluster *clusterv1
 		DiskSetup:                config.Spec.DiskSetup,
 		Mounts:                   config.Spec.Mounts,
 		Files:                    files,
+		ClusterCIDR:              controlPlane.Spec.NetworkSpec.VPC.CidrBlock,
 	}
 	if config.Spec.PauseContainer != nil {
 		nodeInput.PauseContainerAccount = &config.Spec.PauseContainer.AccountNumber
