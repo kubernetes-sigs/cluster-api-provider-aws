c1c0047675a728ea996f94f50cb087cee25255db
serngawy <serngawy@gmail.com>
2025-09-10 07:40:38 -0400

    Fix comments
    Signed-off-by: serngawy <serngawy@gmail.com>

--- exp/api/v1beta2/rosaroleconfig_webhook.go
+++ exp/api/v1beta2/rosaroleconfig_webhook.go
@@ -2,8 +2,12 @@ package v1beta2
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/blang/semver"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	runtime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -29,6 +33,33 @@ var _ webhook.CustomValidator = &rosaRoleConfigWebhook{}
 
 // ValidateCreate implements admission.Validator.
 func (r *rosaRoleConfigWebhook) ValidateCreate(ctx context.Context, obj runtime.Object) (warnings admission.Warnings, err error) {
+	roleConfig, ok := obj.(*ROSARoleConfig)
+	if !ok {
+		return nil, fmt.Errorf("expected an ROSARoleConfig object but got %T", roleConfig)
+	}
+
+	var allErrs field.ErrorList
+	if roleConfig.Spec.OidcProviderType == Managed && roleConfig.Spec.OperatorRoleConfig.OIDCID != "" {
+		err := field.Invalid(field.NewPath("spec.operatorRoleConfig.oidcId"), roleConfig.Spec.OperatorRoleConfig.OIDCID, "cannot be set with Managed oidc provider type")
+		allErrs = append(allErrs, err)
+	} else if roleConfig.Spec.OidcProviderType == Unmanaged && roleConfig.Spec.OperatorRoleConfig.OIDCID == "" {
+		err := field.Invalid(field.NewPath("spec.operatorRoleConfig.oidcId"), roleConfig.Spec.OperatorRoleConfig.OIDCID, "must set operatorRoleConfig.oidcId with UnManaged oidc provider type")
+		allErrs = append(allErrs, err)
+	}
+
+	_, vErr := semver.Parse(roleConfig.Spec.AccountRoleConfig.Version)
+	if vErr != nil {
+		err := field.Invalid(field.NewPath("spec.accountRoleConfig.version"), roleConfig.Spec.AccountRoleConfig.Version, "must be a valid semantic version")
+		allErrs = append(allErrs, err)
+	}
+
+	if len(allErrs) > 0 {
+		return nil, apierrors.NewInvalid(
+			roleConfig.GroupVersionKind().GroupKind(),
+			roleConfig.Name,
+			allErrs)
+	}
+
 	return nil, nil
 }
 
