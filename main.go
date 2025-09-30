19e5940516d77123df1d6badc5c619d65d225a3a
Mohamed ElSerngawy <serngawy@gmail.com>
2025-09-30 13:54:18 +0300

    ✨ Rosa roles config implementations (#5667)
    * Add RosaRoleConfig API and CRD.
    
    * Enable partial reconcile of Rosa Operator Roles
    
    * Review fixes
    
    * Add integration tests
    
    * Add more tests
    
    * Fix comments
    
    Signed-off-by: serngawy <serngawy@gmail.com>
    
    ---------
    
    Signed-off-by: serngawy <serngawy@gmail.com>
    Co-authored-by: rknaur <rknaur@redhat.com>

--- main.go
+++ main.go
@@ -279,8 +279,22 @@ func main() {
 			setupLog.Error(err, "unable to create webhook", "webhook", "ROSAMachinePool")
 			os.Exit(1)
 		}
-	}
 
+		setupLog.Debug("enabling ROSA role config controller")
+		if err = (&expcontrollers.ROSARoleConfigReconciler{
+			Client:           mgr.GetClient(),
+			Recorder:         mgr.GetEventRecorderFor("rosaroleconfig-controller"),
+			WatchFilterValue: watchFilterValue,
+		}).SetupWithManager(ctx, mgr, controller.Options{MaxConcurrentReconciles: awsClusterConcurrency, RecoverPanic: ptr.To[bool](true)}); err != nil {
+			setupLog.Error(err, "unable to create controller", "controller", "ROSARoleConfig")
+			os.Exit(1)
+		}
+
+		if err := (&expinfrav1.ROSARoleConfig{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "ROSARoleConfig")
+			os.Exit(1)
+		}
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddReadyzCheck("webhook", mgr.GetWebhookServer().StartedChecker()); err != nil {
