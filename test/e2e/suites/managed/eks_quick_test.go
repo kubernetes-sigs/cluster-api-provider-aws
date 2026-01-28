9f063b44b6e001c14946f14736a7fe846a356e89
intojhanurag <aojharaj2004@gmail.com>
2026-01-28 09:48:16 +0000

    fix/linting-issue

--- test/e2e/suites/managed/eks_quick_test.go
+++ test/e2e/suites/managed/eks_quick_test.go
@@ -2,33 +2,19 @@
 // +build e2e
 
 /*
-
 Copyright 2026 The Kubernetes Authors.
 
-
-
 Licensed under the Apache License, Version 2.0 (the "License");
-
 you may not use this file except in compliance with the License.
-
 You may obtain a copy of the License at
-
-
 
 	http://www.apache.org/licenses/LICENSE-2.0
 
-
-
 Unless required by applicable law or agreed to in writing, software
-
 distributed under the License is distributed on an "AS IS" BASIS,
-
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
 See the License for the specific language governing permissions and
-
 limitations under the License.
-
 */
 
 package managed
@@ -48,22 +34,16 @@ import (
 	"sigs.k8s.io/cluster-api/util"
 )
 
-var _ = ginkgo.Context("[managed] [EKS] [smoke] [PR-Blocking]", func() {
-
+var _ = ginkgo.Describe("[managed] [EKS] [smoke] [PR-Blocking]", func() {
 	var (
-		namespace *corev1.Namespace
-
-		ctx context.Context
-
+		namespace         *corev1.Namespace
+		ctx               context.Context
 		requiredResources *shared.TestResource
-
-		specName = "eks-quick-start"
-
-		clusterName string
+		specName          = "eks-quick-start"
+		clusterName       string
 	)
 
 	ginkgo.BeforeEach(func() {
-
 		Expect(e2eCtx.Environment.BootstrapClusterProxy).ToNot(BeNil(), "Invalid argument. BootstrapClusterProxy can't be nil")
 
 		ctx = context.TODO()
@@ -71,130 +51,78 @@ var _ = ginkgo.Context("[managed] [EKS] [smoke] [PR-Blocking]", func() {
 		namespace = shared.SetupSpecNamespace(ctx, specName, e2eCtx)
 
 		clusterName = fmt.Sprintf("%s-%s", specName, util.RandomString(6))
-
 	})
 
-	ginkgo.Describe("Running the EKS quick-start spec", func() {
+	ginkgo.BeforeEach(func() {
+		requiredResources = &shared.TestResource{EC2Normal: 2 * e2eCtx.Settings.InstanceVCPU, IGW: 1, NGW: 1, VPC: 1, ClassicLB: 1, EIP: 3, EventBridgeRules: 50}
 
-		ginkgo.BeforeEach(func() {
+		requiredResources.WriteRequestedResources(e2eCtx, "eks-quick-start-test")
 
-			requiredResources = &shared.TestResource{EC2Normal: 2 * e2eCtx.Settings.InstanceVCPU, IGW: 1, NGW: 1, VPC: 1, ClassicLB: 1, EIP: 3, EventBridgeRules: 50}
+		Expect(shared.AcquireResources(requiredResources, ginkgo.GinkgoParallelProcess(), flock.New(shared.ResourceQuotaFilePath))).To(Succeed())
+	})
 
-			requiredResources.WriteRequestedResources(e2eCtx, "eks-quick-start-test")
+	ginkgo.It("should create an EKS cluster with control plane and managed node group", func() {
+		eksClusterName := getEKSClusterName(namespace.Name, clusterName)
 
-			Expect(shared.AcquireResources(requiredResources, ginkgo.GinkgoParallelProcess(), flock.New(shared.ResourceQuotaFilePath))).To(Succeed())
+		ginkgo.By("default iam role should exist")
+		VerifyRoleExistsAndOwned(ctx, ekscontrolplanev1.DefaultEKSControlPlaneRole, eksClusterName, false, e2eCtx.AWSSession)
 
+		ginkgo.By("should create an EKS control plane")
+		ManagedClusterSpec(ctx, func() ManagedClusterSpecInput {
+			return ManagedClusterSpecInput{
+				E2EConfig:                e2eCtx.E2EConfig,
+				ConfigClusterFn:          defaultConfigCluster,
+				BootstrapClusterProxy:    e2eCtx.Environment.BootstrapClusterProxy,
+				AWSSession:               e2eCtx.BootstrapUserAWSSession,
+				Namespace:                namespace,
+				ClusterName:              clusterName,
+				Flavour:                  EKSControlPlaneOnlyFlavor,
+				ControlPlaneMachineCount: 1,
+				WorkerMachineCount:       0,
+			}
 		})
 
-		ginkgo.It("should create an EKS cluster with control plane and managed node group", func() {
-
-			eksClusterName := getEKSClusterName(namespace.Name, clusterName)
-
-			ginkgo.By("default iam role should exist")
-
-			VerifyRoleExistsAndOwned(ctx, ekscontrolplanev1.DefaultEKSControlPlaneRole, eksClusterName, false, e2eCtx.AWSSession)
-
-			ginkgo.By("should create an EKS control plane")
-
-			ManagedClusterSpec(ctx, func() ManagedClusterSpecInput {
-
-				return ManagedClusterSpecInput{
-
-					E2EConfig: e2eCtx.E2EConfig,
-
-					ConfigClusterFn: defaultConfigCluster,
-
-					BootstrapClusterProxy: e2eCtx.Environment.BootstrapClusterProxy,
-
-					AWSSession: e2eCtx.BootstrapUserAWSSession,
-
-					Namespace: namespace,
-
-					ClusterName: clusterName,
-
-					Flavour: EKSControlPlaneOnlyFlavor,
-
-					ControlPlaneMachineCount: 1,
-
-					WorkerMachineCount: 0,
-				}
-
-			})
-
-			ginkgo.By("should create a managed node pool")
-
-			MachinePoolSpec(ctx, func() MachinePoolSpecInput {
-
-				return MachinePoolSpecInput{
-
-					E2EConfig: e2eCtx.E2EConfig,
-
-					ConfigClusterFn: defaultConfigCluster,
-
-					BootstrapClusterProxy: e2eCtx.Environment.BootstrapClusterProxy,
-
-					AWSSession: e2eCtx.BootstrapUserAWSSession,
-
-					Namespace: namespace,
-
-					ClusterName: clusterName,
-
-					IncludeScaling: false,
-
-					Cleanup: false,
-
-					ManagedMachinePool: true,
-
-					Flavor: EKSManagedMachinePoolOnlyFlavor,
-				}
-
-			})
-
-			ginkgo.By(fmt.Sprintf("getting cluster with name %s", clusterName))
-
-			cluster := framework.GetClusterByName(ctx, framework.GetClusterByNameInput{
-
-				Getter: e2eCtx.Environment.BootstrapClusterProxy.GetClient(),
-
-				Namespace: namespace.Name,
-
-				Name: clusterName,
-			})
-
-			Expect(cluster).NotTo(BeNil(), "couldn't find CAPI cluster")
-
-			framework.DeleteCluster(ctx, framework.DeleteClusterInput{
-
-				Deleter: e2eCtx.Environment.BootstrapClusterProxy.GetClient(),
-
-				Cluster: cluster,
-			})
-
-			framework.WaitForClusterDeleted(ctx, framework.WaitForClusterDeletedInput{
-
-				ClusterProxy: e2eCtx.Environment.BootstrapClusterProxy,
-
-				Cluster: cluster,
-
-				ClusterctlConfigPath: e2eCtx.Environment.ClusterctlConfigPath,
-
-				ArtifactFolder: e2eCtx.Settings.ArtifactFolder,
-			}, e2eCtx.E2EConfig.GetIntervals("", "wait-delete-cluster")...)
-
+		ginkgo.By("should create a managed node pool")
+		MachinePoolSpec(ctx, func() MachinePoolSpecInput {
+			return MachinePoolSpecInput{
+				E2EConfig:             e2eCtx.E2EConfig,
+				ConfigClusterFn:       defaultConfigCluster,
+				BootstrapClusterProxy: e2eCtx.Environment.BootstrapClusterProxy,
+				AWSSession:            e2eCtx.BootstrapUserAWSSession,
+				Namespace:             namespace,
+				ClusterName:           clusterName,
+				IncludeScaling:        false,
+				Cleanup:               false,
+				ManagedMachinePool:    true,
+				Flavor:                EKSManagedMachinePoolOnlyFlavor,
+			}
 		})
 
-		ginkgo.AfterEach(func() {
-
-			_ = shared.ReleaseResources(requiredResources, ginkgo.GinkgoParallelProcess(), flock.New(shared.ResourceQuotaFilePath))
-
+		ginkgo.By(fmt.Sprintf("getting cluster with name %s", clusterName))
+		cluster := framework.GetClusterByName(ctx, framework.GetClusterByNameInput{
+			Getter:    e2eCtx.Environment.BootstrapClusterProxy.GetClient(),
+			Namespace: namespace.Name,
+			Name:      clusterName,
 		})
+		Expect(cluster).NotTo(BeNil(), "couldn't find CAPI cluster")
 
+		framework.DeleteCluster(ctx, framework.DeleteClusterInput{
+			Deleter: e2eCtx.Environment.BootstrapClusterProxy.GetClient(),
+			Cluster: cluster,
+		})
+		framework.WaitForClusterDeleted(ctx, framework.WaitForClusterDeletedInput{
+			ClusterProxy:         e2eCtx.Environment.BootstrapClusterProxy,
+			Cluster:              cluster,
+			ClusterctlConfigPath: e2eCtx.Environment.ClusterctlConfigPath,
+			ArtifactFolder:       e2eCtx.Settings.ArtifactFolder,
+		}, e2eCtx.E2EConfig.GetIntervals("", "wait-delete-cluster")...)
 	})
 
 	ginkgo.AfterEach(func() {
-
-		shared.DumpSpecResourcesAndCleanup(ctx, "", namespace, e2eCtx)
-
+		_ = shared.ReleaseResources(requiredResources, ginkgo.GinkgoParallelProcess(), flock.New(shared.ResourceQuotaFilePath))
 	})
 
+	ginkgo.AfterEach(func() {
+		shared.DumpSpecResourcesAndCleanup(ctx, "", namespace, e2eCtx)
+	})
 })
