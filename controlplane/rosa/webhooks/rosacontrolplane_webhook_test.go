9aca4001732507081547d92f050cf62fd1956b67
Matthew Booth <mbooth@redhat.com>
2026-03-03 11:47:35 +0000

    Move webhooks out of API packages
    The primary purpose of this change is to reduce the impact of
    incompatible changes to controller-runtime on packages which import CAPA
    API packages. This change removes webhooks from the api package, and
    their associated c-r dependencies.
    
    This change deliberately follows the webhook pattern used in the
    cluster-api repo. In general this means all webhooks move from api
    packages into a common webhooks package. Each API type which implements
    a webhook gets a corresponding type in the webhooks package which
    implements SetupWebhookWithManager. Internal callers are updated to use
    the new webhook types.
    
    The impact on exported symbols is minimal. The api packages no longer
    export any SetupWebhookWithManager types. Additionally, the
    AWSMachineTemplateWebhook and AWSMachinePoolWebhook types have been
    removed from their corresponding api packages. Apart from that, there
    should be no changes in exported symbols.
    
    Corresponding webhook tests have been moved to the webhooks package.
    
    An explicit goal of this change is to minimise code change during code
    motion. Where possible, code has not been changed at all, just moved.

---
+++ controlplane/rosa/webhooks/rosacontrolplane_webhook_test.go
@@ -0,0 +1,85 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package webhooks
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+
+	rosacontrolplanev1 "sigs.k8s.io/cluster-api-provider-aws/v2/controlplane/rosa/api/v1beta2"
+)
+
+func TestValidateROSANetwork(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	rosaCP := &rosacontrolplanev1.ROSAControlPlane{
+		Spec:   rosacontrolplanev1.RosaControlPlaneSpec{},
+		Status: rosacontrolplanev1.RosaControlPlaneStatus{},
+	}
+
+	w := &ROSAControlPlane{}
+
+	t.Run("Validation error when no ROSANetworkRef, no subnets, no AZs", func(t *testing.T) {
+		err := w.validateROSANetworkRef(rosaCP)
+		g.Expect(err).To(HaveOccurred())
+		g.Expect(err.Error()).To(ContainSubstring("spec.subnets cannot be empty"))
+	})
+
+	t.Run("Validation error when no ROSANetworkRef, subnets present, no AZs", func(t *testing.T) {
+		rosaCP.Spec.Subnets = []string{"subnet01", "subnet02"}
+		err := w.validateROSANetworkRef(rosaCP)
+		g.Expect(err).To(HaveOccurred())
+		g.Expect(err.Error()).To(ContainSubstring("spec.availabilityZones cannot be empty"))
+	})
+
+	t.Run("Validation succeeds when no ROSANetworkRef, subnets and AZs are present", func(t *testing.T) {
+		rosaCP.Spec.AvailabilityZones = []string{"AZ01", "AZ02"}
+		err := w.validateROSANetworkRef(rosaCP)
+		g.Expect(err).NotTo(HaveOccurred())
+	})
+
+	t.Run("Validation error when ROSANetworkRef, subnets and AZs are present", func(t *testing.T) {
+		rosaCP.Spec.ROSANetworkRef = &corev1.LocalObjectReference{}
+		err := w.validateROSANetworkRef(rosaCP)
+		g.Expect(err).To(HaveOccurred())
+		g.Expect(err.Error()).To(ContainSubstring("spec.subnets and spec.rosaNetworkRef are mutually exclusive"))
+	})
+
+	t.Run("Validation error when ROSANetworkRef and subnets are present, no AZs", func(t *testing.T) {
+		rosaCP.Spec.AvailabilityZones = nil
+		err := w.validateROSANetworkRef(rosaCP)
+		g.Expect(err).To(HaveOccurred())
+		g.Expect(err.Error()).To(ContainSubstring("spec.subnets and spec.rosaNetworkRef are mutually exclusive"))
+	})
+
+	t.Run("Validation error when ROSANetworkRef and AZs are present, no subnets", func(t *testing.T) {
+		rosaCP.Spec.AvailabilityZones = []string{"AZ01", "AZ02"}
+		rosaCP.Spec.Subnets = nil
+		err := w.validateROSANetworkRef(rosaCP)
+		g.Expect(err).To(HaveOccurred())
+		g.Expect(err.Error()).To(ContainSubstring("spec.availabilityZones and spec.rosaNetworkRef are mutually exclusive"))
+	})
+
+	t.Run("Validation succeeds when ROSANetworkRef is present, no subnets and no AZs", func(t *testing.T) {
+		rosaCP.Spec.AvailabilityZones = nil
+		rosaCP.Spec.Subnets = nil
+		err := w.validateROSANetworkRef(rosaCP)
+		g.Expect(err).NotTo(HaveOccurred())
+	})
+}
---
+++ exp/webhooks/awsmachinepool_webhook_test.go
@@ -0,0 +1,540 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package webhooks
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+
+	infrav1 "sigs.k8s.io/cluster-api-provider-aws/v2/api/v1beta2"
+	expinfrav1 "sigs.k8s.io/cluster-api-provider-aws/v2/exp/api/v1beta2"
+	utildefaulting "sigs.k8s.io/cluster-api-provider-aws/v2/util/defaulting"
+)
+
+func TestAWSMachinePoolDefault(t *testing.T) {
+	m := &expinfrav1.AWSMachinePool{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"}}
+	t.Run("for AWSCluster", utildefaulting.DefaultValidateTest(context.Background(), m, &AWSMachinePool{}))
+	err := (&AWSMachinePool{}).Default(context.Background(), m)
+	g := NewWithT(t)
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(m.Spec.DefaultCoolDown.Duration).To(BeNumerically(">=", 0))
+}
+
+func TestAWSMachinePoolValidateCreate(t *testing.T) {
+	g := NewWithT(t)
+
+	tests := []struct {
+		name             string
+		pool             *expinfrav1.AWSMachinePool
+		wantErrToContain *string
+	}{
+		{
+			name: "pool with valid tags is accepted",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1": "value-1",
+						"key-2": "value-2",
+					},
+				},
+			},
+			wantErrToContain: nil,
+		},
+		{
+			name: "invalid tags are rejected",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1":                    "value-1",
+						"":                         "value-2",
+						strings.Repeat("CAPI", 33): "value-3",
+						"key-4":                    strings.Repeat("CAPI", 65),
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("additionalTags"),
+		},
+		{
+			name: "Should fail if additional security groups are provided with both ID and Filters",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{AdditionalSecurityGroups: []infrav1.AWSResourceReference{{
+						ID: aws.String("sg-1"),
+						Filters: []infrav1.Filter{
+							{
+								Name:   "sg-1",
+								Values: []string{"test"},
+							},
+						},
+					}}},
+				},
+			},
+			wantErrToContain: ptr.To[string]("filter"),
+		},
+		{
+			name: "Should fail if both subnet ID and filters passed in AWSMachinePool spec",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1": "value-1",
+						"key-2": "value-2",
+					},
+					Subnets: []infrav1.AWSResourceReference{
+						{
+							ID:      ptr.To[string]("subnet-id"),
+							Filters: []infrav1.Filter{{Name: "filter_name", Values: []string{"filter_value"}}},
+						},
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("filter"),
+		},
+		{
+			name: "Should pass if either subnet ID or filters passed in AWSMachinePool spec",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1": "value-1",
+						"key-2": "value-2",
+					},
+					Subnets: []infrav1.AWSResourceReference{
+						{
+							ID: ptr.To[string]("subnet-id"),
+						},
+					},
+				},
+			},
+			wantErrToContain: nil,
+		},
+		{
+			name: "Ensure root volume with device name works (for clusterctl move)",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						RootVolume: &infrav1.Volume{
+							DeviceName: "name",
+							Type:       "gp2",
+							Size:       *aws.Int64(8),
+						},
+					},
+				},
+			},
+			wantErrToContain: nil,
+		},
+		{
+			name: "Should fail if both spot market options or mixed instances policy are set",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					MixedInstancesPolicy: &expinfrav1.MixedInstancesPolicy{
+						Overrides: []expinfrav1.Overrides{{InstanceType: "t3.medium"}},
+					},
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						SpotMarketOptions: &infrav1.SpotMarketOptions{MaxPrice: aws.String("0.1")},
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("spotMarketOptions"),
+		},
+		{
+			name: "Should fail if MaxHealthyPercentage is set, but MinHealthyPercentage is not set",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					RefreshPreferences: &expinfrav1.RefreshPreferences{MaxHealthyPercentage: aws.Int64(100)},
+				},
+			},
+			wantErrToContain: ptr.To[string]("minHealthyPercentage"),
+		},
+		{
+			name: "Should fail if the difference between MaxHealthyPercentage and MinHealthyPercentage is greater than 100",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					RefreshPreferences: &expinfrav1.RefreshPreferences{
+						MaxHealthyPercentage: aws.Int64(150),
+						MinHealthyPercentage: aws.Int64(25),
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("minHealthyPercentage"),
+		},
+		{
+			name: "Should fail if lifecycle hook only has roleARN, but not notificationTargetARN",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLifecycleHooks: []expinfrav1.AWSLifecycleHook{
+						{
+							Name:                "the-hook",
+							LifecycleTransition: expinfrav1.LifecycleHookTransitionInstanceTerminating,
+							RoleARN:             aws.String("role-arn"),
+						},
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("notificationTargetARN"),
+		},
+		{
+			name: "Should fail if lifecycle hook only has notificationTargetARN, but not roleARN",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLifecycleHooks: []expinfrav1.AWSLifecycleHook{
+						{
+							Name:                  "the-hook",
+							LifecycleTransition:   expinfrav1.LifecycleHookTransitionInstanceTerminating,
+							NotificationTargetARN: aws.String("notification-target-arn"),
+						},
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("roleARN"),
+		},
+		{
+			name: "Should fail if the lifecycle hook heartbeat timeout is less than 30 seconds",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLifecycleHooks: []expinfrav1.AWSLifecycleHook{
+						{
+							Name:                  "the-hook",
+							LifecycleTransition:   expinfrav1.LifecycleHookTransitionInstanceTerminating,
+							NotificationTargetARN: aws.String("notification-target-arn"),
+							RoleARN:               aws.String("role-arn"),
+							HeartbeatTimeout:      &metav1.Duration{Duration: 29 * time.Second},
+						},
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("heartbeatTimeout"),
+		},
+		{
+			name: "Should fail if the lifecycle hook heartbeat timeout is more than 172800 seconds",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLifecycleHooks: []expinfrav1.AWSLifecycleHook{
+						{
+							Name:                  "the-hook",
+							LifecycleTransition:   expinfrav1.LifecycleHookTransitionInstanceTerminating,
+							NotificationTargetARN: aws.String("notification-target-arn"),
+							RoleARN:               aws.String("role-arn"),
+							HeartbeatTimeout:      &metav1.Duration{Duration: 172801 * time.Second},
+						},
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("heartbeatTimeout"),
+		},
+		{
+			name: "Should succeed on correct lifecycle hook",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLifecycleHooks: []expinfrav1.AWSLifecycleHook{
+						{
+							Name:                  "the-hook",
+							LifecycleTransition:   expinfrav1.LifecycleHookTransitionInstanceTerminating,
+							NotificationTargetARN: aws.String("notification-target-arn"),
+							RoleARN:               aws.String("role-arn"),
+							HeartbeatTimeout:      &metav1.Duration{Duration: 180 * time.Second},
+						},
+					},
+				},
+			},
+			wantErrToContain: nil,
+		},
+		{
+			name: "with invalid MarketType provided",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						MarketType: "invalid",
+					},
+				},
+			},
+			wantErrToContain: ptr.To("invalid: spec.awsLaunchTemplate.marketType"),
+		},
+		{
+			name: "with MarketType empty value provided",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						MarketType: "",
+					},
+				},
+			},
+			wantErrToContain: nil,
+		},
+		{
+			name: "with MarketType Spot and CapacityReservationID value provided",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						MarketType:            infrav1.MarketTypeSpot,
+						CapacityReservationID: aws.String("cr-123"),
+					},
+				},
+			},
+			wantErrToContain: ptr.To("cannot be set to 'Spot' when CapacityReservationID is specified"),
+		},
+		{
+			name: "with CapacityReservationID and SpotMarketOptions value provided",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						SpotMarketOptions:     &infrav1.SpotMarketOptions{},
+						CapacityReservationID: aws.String("cr-123"),
+					},
+				},
+			},
+			wantErrToContain: ptr.To("cannot be set to when CapacityReservationID is specified"),
+		},
+		{
+			name: "with CapacityReservationPreference of `none` and CapacityReservationID is specified",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						CapacityReservationID:         aws.String("cr-123"),
+						CapacityReservationPreference: infrav1.CapacityReservationPreferenceNone,
+					},
+				},
+			},
+			wantErrToContain: ptr.To("when capacityReservationId is specified, capacityReservationPreference may only be `CapacityReservationsOnly` or empty"),
+		},
+		{
+			name: "invalid, MarketType set to MarketTypeCapacityBlock and spotMarketOptions are specified",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						MarketType:        infrav1.MarketTypeCapacityBlock,
+						SpotMarketOptions: &infrav1.SpotMarketOptions{},
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("setting marketType to CapacityBlock and spotMarketOptions cannot be used together"),
+		},
+		{
+			name: "invalid, MarketType set to MarketTypeOnDemand and spotMarketOptions are specified",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						MarketType:        infrav1.MarketTypeOnDemand,
+						SpotMarketOptions: &infrav1.SpotMarketOptions{},
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("setting marketType to OnDemand and spotMarketOptions cannot be used together"),
+		},
+		{
+			name: "valid MarketType set to MarketTypeCapacityBlock is specified and CapacityReservationId is not provided",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						MarketType: infrav1.MarketTypeCapacityBlock,
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("capacityReservationID: Forbidden: is required when CapacityBlock is provided"),
+		},
+		{
+			name: "valid MarketType set to MarketTypeCapacityBlock and CapacityReservationId are specified",
+			pool: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						MarketType:            infrav1.MarketTypeCapacityBlock,
+						CapacityReservationID: aws.String("cr-12345678901234567"),
+					},
+				},
+			},
+			wantErrToContain: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			warn, err := (&AWSMachinePool{}).ValidateCreate(context.Background(), tt.pool)
+			if tt.wantErrToContain != nil {
+				g.Expect(err).ToNot(BeNil())
+				if err != nil {
+					g.Expect(err.Error()).To(ContainSubstring(*tt.wantErrToContain))
+				}
+			} else {
+				g.Expect(err).To(Succeed())
+			}
+			// Nothing emits warnings yet
+			g.Expect(warn).To(BeEmpty())
+		})
+	}
+}
+
+func TestAWSMachinePoolValidateUpdate(t *testing.T) {
+	g := NewWithT(t)
+
+	tests := []struct {
+		name             string
+		new              *expinfrav1.AWSMachinePool
+		old              *expinfrav1.AWSMachinePool
+		wantErrToContain *string
+	}{
+		{
+			name: "adding tags is accepted",
+			old: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1": "value-1",
+					},
+				},
+			},
+			new: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1": "value-1",
+						"key-2": "value-2",
+					},
+				},
+			},
+			wantErrToContain: nil,
+		},
+		{
+			name: "adding invalid tags is rejected",
+			old: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1": "value-1",
+					},
+				},
+			},
+			new: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1":                    "value-1",
+						"":                         "value-2",
+						strings.Repeat("CAPI", 33): "value-3",
+						"key-4":                    strings.Repeat("CAPI", 65),
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("additionalTags"),
+		},
+		{
+			name: "Should fail update if both subnetID and filters passed in AWSMachinePool spec",
+			old: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1": "value-1",
+					},
+				},
+			},
+			new: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1": "value-1",
+						"key-2": "value-2",
+					},
+					Subnets: []infrav1.AWSResourceReference{
+						{
+							ID:      ptr.To[string]("subnet-id"),
+							Filters: []infrav1.Filter{{Name: "filter_name", Values: []string{"filter_value"}}},
+						},
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("filter"),
+		},
+		{
+			name: "Should pass update if either subnetID or filters passed in AWSMachinePool spec",
+			old: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1": "value-1",
+					},
+				},
+			},
+			new: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					AdditionalTags: infrav1.Tags{
+						"key-1": "value-1",
+						"key-2": "value-2",
+					},
+					Subnets: []infrav1.AWSResourceReference{
+						{
+							ID: ptr.To[string]("subnet-id"),
+						},
+					},
+				},
+			},
+			wantErrToContain: nil,
+		},
+		{
+			name: "Should fail update if both spec.awsLaunchTemplate.SpotMarketOptions and spec.MixedInstancesPolicy are passed in AWSMachinePool spec",
+			old: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					MixedInstancesPolicy: &expinfrav1.MixedInstancesPolicy{
+						Overrides: []expinfrav1.Overrides{{InstanceType: "t3.medium"}},
+					},
+				},
+			},
+			new: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					MixedInstancesPolicy: &expinfrav1.MixedInstancesPolicy{
+						Overrides: []expinfrav1.Overrides{{InstanceType: "t3.medium"}},
+					},
+					AWSLaunchTemplate: expinfrav1.AWSLaunchTemplate{
+						SpotMarketOptions: &infrav1.SpotMarketOptions{MaxPrice: ptr.To[string]("0.1")},
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("spotMarketOptions"),
+		},
+		{
+			name: "Should fail if MaxHealthyPercentage is set, but MinHealthyPercentage is not set",
+			new: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					RefreshPreferences: &expinfrav1.RefreshPreferences{MaxHealthyPercentage: aws.Int64(100)},
+				},
+			},
+			wantErrToContain: ptr.To[string]("minHealthyPercentage"),
+		},
+		{
+			name: "Should fail if the difference between MaxHealthyPercentage and MinHealthyPercentage is greater than 100",
+			new: &expinfrav1.AWSMachinePool{
+				Spec: expinfrav1.AWSMachinePoolSpec{
+					RefreshPreferences: &expinfrav1.RefreshPreferences{
+						MaxHealthyPercentage: aws.Int64(150),
+						MinHealthyPercentage: aws.Int64(25),
+					},
+				},
+			},
+			wantErrToContain: ptr.To[string]("minHealthyPercentage"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			warn, err := (&AWSMachinePool{}).ValidateUpdate(context.Background(), tt.old.DeepCopy(), tt.new)
+			if tt.wantErrToContain != nil {
+				g.Expect(err).ToNot(BeNil())
+				if err != nil {
+					g.Expect(err.Error()).To(ContainSubstring(*tt.wantErrToContain))
+				}
+			} else {
+				g.Expect(err).To(Succeed())
+			}
+			// Nothing emits warnings yet
+			g.Expect(warn).To(BeEmpty())
+		})
+	}
+}
