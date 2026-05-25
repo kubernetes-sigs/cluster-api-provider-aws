1a7e6aca51bb73485a5361e95b8f55569d77d4a7
Wendy Ha <wendyha.sut@gmail.com>
2026-05-26 07:37:23 +1000

    fix: Update fill list of regions to generate ami inventory report
    Signed-off-by: Wendy Ha <wendyha.sut@gmail.com>

--- scripts/generate-ami-inventory-report.sh
+++ scripts/generate-ami-inventory-report.sh
@@ -25,7 +25,7 @@ set -euo pipefail
 # --- Constants --- 
 
 AWS_ACCOUNT_ID="819546954734"
-DEFAULT_REGIONS="us-east-2"
+DEFAULT_REGIONS="ap-south-1,eu-west-3,eu-west-2,eu-west-1,ap-northeast-2,ap-northeast-1,sa-east-1,ca-central-1,ap-southeast-1,ap-southeast-2,eu-central-1,us-east-1,us-east-2,us-west-1,us-west-2"
 DEFAULT_OS="ubuntu-24.04,ubuntu-22.04,flatcar-stable"
 
 TARGET_FILE="docs/book/src/development/ami-inventory.md"
